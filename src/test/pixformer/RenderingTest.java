96d6e6b5a6ea7d845d4f4b01ab10853b8a04561c
Giorgio Garofalo <giogar2705@gmail.com>
2022-12-31 02:17:25 +0100

    Add rendering test application

---
+++ src/test/pixformer/RenderingTest.java
@@ -0,0 +1,34 @@
+package pixformer;
+
+import javafx.application.Application;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+import pixformer.view.Color;
+import pixformer.view.RendererFactory;
+import pixformer.view.TextBuilder;
+import pixformer.view.javafx.JavaFXImageRenderer;
+import pixformer.view.javafx.JavaFXScene;
+
+/**
+ * Test for the rendering architecture.
+ */
+public class RenderingTest extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        JavaFXScene scene = new JavaFXScene();
+        RendererFactory factory = scene.getRendererFactory();
+
+        scene.getRenderer().add(factory.newSolidBackground(new Color(.4, .3, .6)));
+        scene.getRenderer().add(new JavaFXImageRenderer(new Image("https://dev.java/assets/images/java-logo-vert-blk.png"))); // TODO add to factory
+        scene.getRenderer().add(new TextBuilder("Test").withColor(Color.WHITE).withFamily("Monospaced").build(factory).at(100, 100));
+        scene.render();
+
+        primaryStage.setScene(scene.getScene());
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
