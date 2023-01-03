b3386526ff6510a7b41778e4d19d006f9c735fdf
Giorgio Garofalo <giogar2705@gmail.com>
2023-01-03 19:47:38 +0100

    Add support for tileset-style image rendering

--- src/main/java/pixformer/view/engine/javafx/JavaFXImageRenderer.java
+++ src/main/java/pixformer/view/engine/javafx/JavaFXImageRenderer.java
@@ -10,13 +10,33 @@ import pixformer.view.engine.PositionableRenderer;
 public class JavaFXImageRenderer extends PositionableRenderer {
 
     private final Image image;
+    private final double width;
+    private final double height;
+    private final double sourceX;
+    private final double sourceY;
+
+    /**
+     * Creates a static image renderer for JavaFX
+     * @param image image to render
+     * @param width image width
+     * @param height image height
+     * @param sourceX starting X coordinate of the image
+     * @param sourceY starting Y coordinate of the image
+     */
+    public JavaFXImageRenderer(final Image image, final double width, final double height, final double sourceX, final double sourceY) {
+        this.image = image;
+        this.width = width;
+        this.height = height;
+        this.sourceX = sourceX;
+        this.sourceY = sourceY;
+    }
 
     /**
      * Creates a static image renderer for JavaFX
      * @param image image to render
      */
     public JavaFXImageRenderer(final Image image) {
-        this.image = image;
+        this(image, image.getWidth(), image.getHeight(), 0D, 0D);
     }
 
     /**
@@ -24,6 +44,9 @@ public class JavaFXImageRenderer extends PositionableRenderer {
      */
     @Override
     public void render(final double dt, final Graphics graphics) {
-        JavaFXGraphics.requireJFXGraphics(graphics).getGraphics().drawImage(image, super.getX(), super.getY());
+        JavaFXGraphics.requireJFXGraphics(graphics).getGraphics().drawImage(image,
+                this.sourceX, this.sourceY, this.width, this.height,
+                super.getX(), super.getY(), this.width, this.height
+        );
     }
 }
