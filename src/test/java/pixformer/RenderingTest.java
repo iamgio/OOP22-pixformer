efc15d6047d3b7429254c9fd31059b5a8e7ba960
Giorgio Garofalo <giogar2705@gmail.com>
2023-01-05 23:22:54 +0100

    Fix RenderingTest

--- src/test/java/pixformer/RenderingTest.java
+++ src/test/java/pixformer/RenderingTest.java
@@ -4,8 +4,9 @@ import javafx.application.Application;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import pixformer.view.engine.Color;
+import pixformer.view.engine.RectangleRenderer;
 import pixformer.view.engine.RendererFactory;
-import pixformer.view.engine.TextBuilder;
+import pixformer.view.engine.TextRenderer;
 import pixformer.view.engine.javafx.JavaFXImageRenderer;
 import pixformer.view.engine.javafx.JavaFXScene;
 
@@ -21,15 +22,18 @@ public class RenderingTest extends Application {
 
         scene.add(factory.newSolidBackground(new Color(.4, .3, .6)));
         scene.add(new JavaFXImageRenderer(new Image("https://dev.java/assets/images/java-logo-vert-blk.png"))); // TODO add to factory
-        scene.add(new TextBuilder("Test").withColor(Color.WHITE).withFamily("Monospaced").build(factory).at(100, 100));
-        scene.add(factory.newRectangle(300, 200, new Color(.8, .5, .5)).at(400, 400));
+
+        TextRenderer text = factory.newText("Test");
+        text.setColor(Color.WHITE);
+        text.setFontFamily("Monospaced");
+        scene.add(text.at(100, 100));
+
+        RectangleRenderer rectangle = factory.newRectangle(300, 200);
+        rectangle.setColor(new Color(.8, .5, .5));
+        scene.add(rectangle.at(400, 400));
         scene.render();
 
         primaryStage.setScene(scene.getScene());
         primaryStage.show();
-    }
-
-    public static void main(String[] args) {
-        launch(args);
     }
 }
---
+++ src/test/java/pixformer/RenderingTestStarter.java
@@ -0,0 +1,12 @@
+package pixformer;
+
+import javafx.application.Application;
+
+/**
+ *
+ */
+public class RenderingTestStarter {
+    public static void main(String[] args) {
+        Application.launch(RenderingTest.class, args);
+    }
+}
