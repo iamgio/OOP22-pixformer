37abbcceac27e117f01d418a804a1da4be6030ac
NickGhignatti <nick.ghignatti@gmail.com>
2023-02-02 00:07:04 +0100

    add interface for entities

---
+++ src/main/java/pixformer/model/entity/Entity.java
@@ -0,0 +1,17 @@
+package pixformer.model.entity;
+
+/**
+ * In-Game entity
+ */
+public interface Entity {
+
+    // TODO : have to create the GraphicsComponent that the entity should return
+    // something like this :
+    // GraphicsComponent getGraphicsComponent();
+
+    // TODO : have to expose the position of the entity, Vector2D || Point2d || Something else
+    // Vector2D<T> getVector2D();
+    // Point2D<T> getPoint2D();
+
+    // TODO : other methods that are needed
+}
