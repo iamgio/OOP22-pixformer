fb39fe9695ad2f49cc4e91252a4d56c01205f917
Luca Patrignani <luca.patrignani3@studio.unibo.it>
2023-02-05 16:29:50 +0100

    Remove any implementation details from Point2D and Vector2D

--- src/main/java/pixformer/common/Point2D.java
+++ src/main/java/pixformer/common/Point2D.java
@@ -6,28 +6,6 @@ package pixformer.common;
 public interface Point2D {
 
     /**
-     * @param x
-     * @param y
-     * @return a new Point2D whose x component is {@code x} and y component is
-     *         {@code y}.
-     */
-    static Point2D of(final double x, final double y) {
-        return new Point2D() {
-
-            @Override
-            public double getX() {
-                return x;
-            }
-
-            @Override
-            public double getY() {
-                return y;
-            }
-
-        };
-    }
-
-    /**
      * @return the X component
      */
     double getX();
--- src/main/java/pixformer/common/Vector2D.java
+++ src/main/java/pixformer/common/Vector2D.java
@@ -6,44 +6,13 @@ package pixformer.common;
 public interface Vector2D extends Point2D {
 
     /**
-     * @param point
-     * @return a new Vector2D whose components are the same of {@code point}.
-     */
-    static Vector2D fromPoint(final Point2D point) {
-        return new Vector2D() {
-
-            @Override
-            public double getX() {
-                return point.getX();
-            }
-
-            @Override
-            public double getY() {
-                return point.getY();
-            }
-
-        };
-    }
-
-    /**
-     * @param x
-     * @param y
-     * @return a new Vector2D of components {@code x} and {@code y}.
-     */
-    static Vector2D of(final double x, final double y) {
-        return fromPoint(Point2D.of(x, y));
-    }
-
-    /**
      * Sum of two vectors.
      * 
      * @param other
      * @return a new Vector2D whose components are the sum of the components of
      *         {@code this} and {@code other}.
      */
-    default Vector2D sum(final Vector2D other) {
-        return of(this.getX() + other.getX(), this.getY() + other.getY());
-    }
+    Vector2D sum(final Vector2D other);
 
     /**
      * Product between a vector and a scalar value.
@@ -52,8 +21,6 @@ public interface Vector2D extends Point2D {
      * @return a new Vector2D whose components are the product of the components of
      *         {@code this} and {@code k}.
      */
-    default Vector2D scale(final double k) {
-        return of(this.getX() * k, this.getY() * k);
-    }
+    Vector2D scale(final double k);
 
 }
