3cad11826834b70677137e4251a231237b7c3fa6
Luca Patrignani <luca.patrignani3@studio.unibo.it>
2023-02-05 15:04:42 +0100

    Remove the add method from Vector2D

--- src/main/java/pixformer/common/Vector2D.java
+++ src/main/java/pixformer/common/Vector2D.java
@@ -5,10 +5,4 @@ package pixformer.common;
  */
 public interface Vector2D extends Point2D {
 
-    /**
-     * Append a new Vector2D at the end of this Vector.
-     * @param v Vector2D to append
-     */
-    void add(Vector2D v);
-
 }
