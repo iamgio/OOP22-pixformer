ee11f0e2ecc2653845d0eeca2a09897b07ff49af
Luca Patrignani <luca.patrignani3@studio.unibo.it>
2023-02-05 15:04:02 +0100

    Make Vector2D a subinterface of Point2D

--- src/main/java/pixformer/common/Vector2D.java
+++ src/main/java/pixformer/common/Vector2D.java
@@ -3,7 +3,7 @@ package pixformer.common;
 /**
  * Double 2D Vector.
  */
-public interface Vector2D {
+public interface Vector2D extends Point2D {
 
     /**
      * Append a new Vector2D at the end of this Vector.
@@ -11,13 +11,4 @@ public interface Vector2D {
      */
     void add(Vector2D v);
 
-    /**
-     * @return the X component
-     */
-    double getX();
-
-    /**
-     * @return the Y component
-     */
-    double getY();
 }
