097ac4848ec9cf34ac1273cb4ac59d5e209df1a9
NickGhignatti <nick.ghignatti@gmail.com>
2023-02-06 18:08:49 +0100

    Add methods signatures for Entity

--- src/main/java/pixformer/model/entity/Entity.java
+++ src/main/java/pixformer/model/entity/Entity.java
@@ -5,4 +5,24 @@ package pixformer.model.entity;
  */
 public interface Entity {
 
+    /**
+     * @return X coordinate
+     */
+    double getX();
+
+    /**
+     * @return Y coordinate
+     */
+    double getY();
+
+    /**
+     * @return the width of the entity
+     */
+    double getWidth();
+
+    /**
+     * @return the height of the entity
+     */
+    double getHeight();
+
 }
