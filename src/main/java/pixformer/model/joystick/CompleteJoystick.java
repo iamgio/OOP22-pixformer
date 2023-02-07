5775126da07fc8a43204d008843d8099a3925fd3
Luca Patrignani <luca.patrignani3@studio.unibo.it>
2023-02-07 17:39:18 +0100

    Create a Joystick interface which allows all ordinary movements

---
+++ src/main/java/pixformer/model/joystick/CompleteJoystick.java
@@ -0,0 +1,12 @@
+package pixformer.model.joystick;
+
+/**
+ * A Joystick which allows all classic Mario movements.
+ */
+public interface CompleteJoystick extends
+        HorizontalJoystick,
+        FireJoystick,
+        JumpJoystick,
+        CrouchJoystick {
+
+}
