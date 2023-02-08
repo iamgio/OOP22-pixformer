283fed5c2f29620d526267d48c82efbc8befbd6f
Luca Patrignani <luca.patrignani3@studio.unibo.it>
2023-02-08 16:44:22 +0100

    Add docs

--- src/main/java/pixformer/view/Command.java
+++ src/main/java/pixformer/view/Command.java
@@ -5,6 +5,12 @@ import java.util.function.Consumer;
 import pixformer.controller.input.ControllerInputComponent;
 import pixformer.model.joystick.Joystick;
 
+/**
+ * It's an abstraction of command produced by a {@link pixformer.view.Command}
+ * which will be collected by the controller.
+ * 
+ * @param <J> the joystick which will might receive the the command.
+ */
 public interface Command<J extends Joystick> extends Consumer<ControllerInputComponent<J>> {
-    
+
 }
