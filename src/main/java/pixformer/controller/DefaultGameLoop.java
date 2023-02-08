4d9de598664c2ec012b247f3f0c4de99f3ed6942
Luca Patrignani <luca.patrignani3@studio.unibo.it>
2023-02-08 16:13:29 +0100

    Change the game loop method implementation for explanatory purposes

--- src/main/java/pixformer/controller/DefaultGameLoop.java
+++ src/main/java/pixformer/controller/DefaultGameLoop.java
@@ -1,21 +1,51 @@
 package pixformer.controller;
 
+import java.util.function.Consumer;
+
+import pixformer.controller.input.ControllerInputComponent;
+import pixformer.model.ModelInputComponent;
+import pixformer.model.joystick.CompleteJoystick;
 import pixformer.view.View;
+import pixformer.view.ViewInputComponent;
 
 /**
  * The default game loop.
  */
 public class DefaultGameLoop implements GameLoop {
 
+    private final ViewInputComponent viewInput;
     private final View view;
+    private final ModelInputComponent model;
+    private boolean isRunning = true;
+    private final ControllerInputComponent<CompleteJoystick> mockInputComponent = new ControllerInputComponent<CompleteJoystick>() {
+
+        @Override
+        public void acceptGameInput(Consumer<CompleteJoystick> input) {
+            model.acceptMarioInput(input);
+        }
+
+        @Override
+        public void pause() {
+            isRunning = false;
+        }
+
+        @Override
+        public void unpause() {
+            isRunning = true;
+        }
+
+    };
 
     /**
      * Creates the default game loop that relies on a visual renderable scene.
+     * 
      * @param view active game view
      */
-    public DefaultGameLoop(final View view) {
+    public DefaultGameLoop(final ViewInputComponent viewComp, final ModelInputComponent model, final View view) {
+        this.viewInput = viewComp;
+        this.model = model;
         this.view = view;
-        this.view.setup();
+        view.setup();
     }
 
     /**
@@ -23,6 +53,11 @@ public class DefaultGameLoop implements GameLoop {
      */
     @Override
     public void loop(final long now) {
+        var optional = viewInput.popInput();
+        if (optional.isPresent()) {
+            var input = optional.get();
+            input.accept(mockInputComponent);
+        }
         this.view.update(0 /* TODO delta time */);
     }
 }
