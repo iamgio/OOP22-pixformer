fcef2a8aa52ef5bf3d66fe6c8a43ea04480d82ca
Luca Patrignani <luca.patrignani3@studio.unibo.it>
2023-02-08 16:29:39 +0100

    Add docs

--- src/main/java/pixformer/controller/input/ControllerInputComponent.java
+++ src/main/java/pixformer/controller/input/ControllerInputComponent.java
@@ -4,11 +4,29 @@ import java.util.function.Consumer;
 
 import pixformer.model.joystick.Joystick;
 
+/**
+ * An interface which represents a component of the controller which will
+ * accepts calls from a {@link pixformer.view.viewInputComponent}.
+ * 
+ * @param <J> the Joystick type which this class will accept calls.
+ */
 public interface ControllerInputComponent<J extends Joystick> {
 
+    /**
+     * Accept an input related to game actions.
+     * 
+     * @param input a call to the joystick connected to this
+     *              {@code ControllerInputComponent} object.
+     */
     void acceptGameInput(Consumer<J> input);
 
+    /**
+     * pause the game.
+     */
     void pause();
 
+    /**
+     * Unpause the game.
+     */
     void unpause();
 }
