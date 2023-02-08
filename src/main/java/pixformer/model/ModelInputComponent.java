8e0e86c1cc8d60514c4e37595d818760fb868227
Luca Patrignani <luca.patrignani3@studio.unibo.it>
2023-02-08 16:34:27 +0100

    Add docs

--- src/main/java/pixformer/model/ModelInputComponent.java
+++ src/main/java/pixformer/model/ModelInputComponent.java
@@ -4,8 +4,18 @@ import java.util.function.Consumer;
 
 import pixformer.model.joystick.CompleteJoystick;
 
-
+/**
+ * An interface responsible of accepting input for the model.
+ * For each controllable entity in the game there should be one instance of this
+ * interface.
+ */
 public interface ModelInputComponent {
-    
+
+    /**
+     * Accept an input for a controllable entity in the game.
+     * 
+     * @param input a call to a joystick indicating the action which shall be
+     *              performed by the entity.
+     */
     void acceptMarioInput(Consumer<CompleteJoystick> input);
 }
