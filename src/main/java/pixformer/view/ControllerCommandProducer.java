7bc1db8cdcf2775a39273341dc4fa9b1eac8b571
Luca Patrignani <luca.patrignani3@studio.unibo.it>
2023-02-09 16:04:44 +0100

    Create ControllerCommandProducer interface

---
+++ src/main/java/pixformer/view/ControllerCommandProducer.java
@@ -0,0 +1,22 @@
+package pixformer.view;
+
+import java.util.Optional;
+import java.util.function.Consumer;
+
+import pixformer.controller.input.ControllerInput;
+
+/**
+ * It represents something which can produce commands for the controller.
+ */
+public interface ControllerCommandProducer<C extends ControllerInput> {
+    
+    /**
+     * It pops the possibly-collected command.
+     * 
+     * @return the command.
+     * @implNote when this method is called, a following call of this method should
+     *           return {@code Optional.empty()} if the view hasn't received another
+     *           input in the meanwhile.
+     */
+    Optional<Consumer<C>> popControllerCommand();
+}
