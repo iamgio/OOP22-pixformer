19d4279ae26d65a5ea78e7a1d0f4f703b90a7273
Luca Patrignani <luca.patrignani3@studio.unibo.it>
2023-02-09 15:21:48 +0100

    Create ControllerInput

---
+++ src/main/java/pixformer/controller/input/ControllerInput.java
@@ -0,0 +1,9 @@
+package pixformer.controller.input;
+
+/**
+ * A tagging interface which represents a command to the controller.
+ * All commands to the controller shall extend this interface.
+ */
+public interface ControllerInput {
+
+}
