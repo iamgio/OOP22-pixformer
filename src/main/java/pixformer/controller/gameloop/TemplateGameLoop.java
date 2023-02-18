7656bb6d1efe1c69e30a56a2efe5ce4b90ff5cdf
Luca Patrignani <luca.patrignani3@studio.unibo.it>
2023-02-18 18:34:54 +0100

    Create TemplateGameLoop

---
+++ src/main/java/pixformer/controller/gameloop/TemplateGameLoop.java
@@ -0,0 +1,43 @@
+package pixformer.controller.gameloop;
+
+
+/**
+ * A game loop with fixed operation to be performed. 
+ */
+public abstract class TemplateGameLoop implements GameLoop {
+
+    private double previous;
+
+    /**
+     * When invoked, makes the game loop thread sleep for a certain amount of time.
+     * 
+     * @param dt the delta time between the previous iteration and the current one.
+     */
+    abstract void sleep(double dt);
+
+    /**
+     * When invoked the game loop processes the input of the user.
+     */
+    abstract void processInput();
+
+    /**
+     * Update the model.
+     * @param dt dt the delta time between the previous iteration and the current one.
+     */
+    abstract void update(double dt);
+
+    /**
+     * Update the view.
+     */
+    abstract void render();
+
+    @Override
+    public void loop(final long now) {
+        final double elapsed = now - previous;
+        processInput();
+        update(elapsed);
+        render();
+        sleep(elapsed);
+    }
+
+}
