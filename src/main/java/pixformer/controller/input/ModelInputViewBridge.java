18ad514d8c3c88313e9e5361b7f001c014b970c8
Giorgio Garofalo <giogar2705@gmail.com>
2023-02-24 19:22:16 +0100

    Fix merge naming issue

--- src/main/java/pixformer/controller/input/ModelInputViewBridge.java
+++ src/main/java/pixformer/controller/input/ModelInputViewBridge.java
@@ -37,7 +37,7 @@ public final class ModelInputViewBridge implements CompleteModelInput {
      * {@inheritDoc}
      */
     @Override
-    public void fire() {
+    public void ability() {
         // TODO
     }
 
