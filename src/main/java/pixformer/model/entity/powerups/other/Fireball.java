ebc198cebbfd8da940d31014f77b028610b4eecb
IGieckI <giacomo.antonelli3@gmail.com>
2023-02-26 16:01:32 +0100

    Implemented class Fireball

--- src/main/java/pixformer/model/entity/powerups/other/Fireball.java
+++ src/main/java/pixformer/model/entity/powerups/other/Fireball.java
@@ -1,43 +1,32 @@
 package pixformer.model.entity.powerups.other;
 
 import pixformer.common.Updatable;
-import pixformer.model.entity.DrawableEntity;
-import pixformer.model.entity.GraphicsComponent;
+import pixformer.common.Vector2D;
+import pixformer.model.entity.AbstractDrawableEntity;
 
-public class Fireball implements DrawableEntity, Updatable {
+public class Fireball extends AbstractDrawableEntity implements Updatable {
+
+    final double SPEED = 1.0;
+
+    public Fireball(double x, double y, double width, double height, Direction direction){
+        super(x,y,width,height);
+        this.direction = direction;
+    }
 
     @Override
     public void update(double dt) {
-        
-    }
+        int movement = 0;
 
-    @Override
-    public double getX() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getX'");
-    }
 
-    @Override
-    public double getY() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getY'");
-    }
+        if (this.leftKey) {
+            movement--;
+        }
 
-    @Override
-    public double getWidth() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getWidth'");
-    }
 
-    @Override
-    public double getHeight() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getHeight'");
-    }
+        if (this.rightKey) {
+            movement++;
+        }
 
-    @Override
-    public GraphicsComponent getGraphicsComponent() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getGraphicsComponent'");
+        this.position = this.position.sum(new Vector2D(SPEED * dt * movement, 0));
     }    
 }
