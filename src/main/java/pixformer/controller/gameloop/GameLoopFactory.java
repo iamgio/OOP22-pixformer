96c20a20adb7f2f01cb32b526ee36e0edd5e9ed1
Giorgio Garofalo <giogar2705@gmail.com>
2023-03-03 17:04:13 +0100

    Use classic game loop

--- src/main/java/pixformer/controller/gameloop/GameLoopFactory.java
+++ src/main/java/pixformer/controller/gameloop/GameLoopFactory.java
@@ -34,26 +34,23 @@ public final class GameLoopFactory {
 
         final World world = this.level.getWorld();
 
-        return new GeneralGameLoop(
-                () -> {},
-                world::update,
-                () -> {
-                    view.update(0);
+        return dt -> {
+            view.update(dt);
+            world.update(dt);
 
-                    world.getEntities().forEach(entity -> {
-                        view.getScene().getGraphics().setTranslate(entity.getX(), entity.getY());
-                        entity.getGraphicsComponent().update(view.getScene());
-                    });
-                },
-                dt -> {
-                    final long period = (SECONDS_TO_MILLIS / FPS);
-                    if (dt < period) {
-                        try {
-                            Thread.sleep(period - dt);
-                        } catch (final InterruptedException ex) {
-                            ex.printStackTrace();
-                        }
-                    }
-                });
+            world.getEntities().forEach(entity -> {
+                view.getScene().getGraphics().setTranslate(entity.getX(), entity.getY());
+                entity.getGraphicsComponent().update(view.getScene());
+            });
+
+            final long period = SECONDS_TO_MILLIS / FPS;
+            if (dt < period) {
+                try {
+                    Thread.sleep(period - dt);
+                } catch (final InterruptedException ex) {
+                    ex.printStackTrace();
+                }
+            }
+        };
     }
 }
