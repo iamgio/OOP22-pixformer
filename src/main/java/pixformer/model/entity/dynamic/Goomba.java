31153480b7e8c24737c00967e882993d9467ef38
Luca Patrignani <luca.patrignani3@studio.unibo.it>
2023-03-07 18:41:02 +0100

    Add docs

--- src/main/java/pixformer/model/entity/dynamic/Goomba.java
+++ src/main/java/pixformer/model/entity/dynamic/Goomba.java
@@ -7,7 +7,9 @@ import pixformer.model.entity.AbstractEntity;
 import pixformer.model.entity.collision.DefaultRectangleBoundingBoxEntity;
 import pixformer.model.entity.dynamic.ai.GoombaAI;
 import pixformer.model.input.InputComponent;
-
+/**
+ * The default implementation of a goomba.
+ */
 public class Goomba extends AbstractEntity implements DefaultRectangleBoundingBoxEntity {
 
     private static final int WIDTH = 1;
@@ -15,6 +17,11 @@ public class Goomba extends AbstractEntity implements DefaultRectangleBoundingBo
     private static final Vector2D INITIAL_VELOCITY = new Vector2D(-0.04, 0);
     private final GoombaAI ai;
 
+    /**
+     * Create a Goomba at position ({@code x}, {@code y}).
+     * @param x coordinate
+     * @param y coordinate.
+     */
     public Goomba(final double x, final double y) {
         super(x, y, WIDTH, HEIGHT);
         setVelocity(INITIAL_VELOCITY);
