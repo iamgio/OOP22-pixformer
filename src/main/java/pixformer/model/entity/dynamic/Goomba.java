5408e6737012ebdfecfeae946145a0d91d08f331
Luca Patrignani <luca.patrignani3@studio.unibo.it>
2023-03-07 18:41:52 +0100

    Remove trailing spaces

--- src/main/java/pixformer/model/entity/dynamic/Goomba.java
+++ src/main/java/pixformer/model/entity/dynamic/Goomba.java
@@ -1,12 +1,12 @@
 package pixformer.model.entity.dynamic;
 
 import java.util.Optional;
-
 import pixformer.common.Vector2D;
 import pixformer.model.entity.AbstractEntity;
 import pixformer.model.entity.collision.DefaultRectangleBoundingBoxEntity;
 import pixformer.model.entity.dynamic.ai.GoombaAI;
 import pixformer.model.input.InputComponent;
+
 /**
  * The default implementation of a goomba.
  */
