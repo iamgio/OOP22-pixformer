ccd0472d5ad322d9c6e255ca88b4e9a9cb164ec2
Luca Patrignani <luca.patrignani3@studio.unibo.it>
2023-03-07 17:08:27 +0100

    Add initial velocity to goomba

--- src/main/java/pixformer/model/entity/dynamic/Goomba.java
+++ src/main/java/pixformer/model/entity/dynamic/Goomba.java
@@ -2,6 +2,7 @@ package pixformer.model.entity.dynamic;
 
 import java.util.Optional;
 
+import pixformer.common.Vector2D;
 import pixformer.model.entity.AbstractEntity;
 import pixformer.model.entity.collision.DefaultRectangleBoundingBoxEntity;
 import pixformer.model.entity.dynamic.ai.GoombaAI;
@@ -11,10 +12,12 @@ public class Goomba extends AbstractEntity implements DefaultRectangleBoundingBo
 
     private static int WIDTH = 1;
     private static int HEIGHT = 1;
+    private static final Vector2D INITIAL_VELOCITY = new Vector2D(-0.04, 0);
     private final GoombaAI ai;
 
     public Goomba(final double x, final double y) {
         super(x, y, WIDTH, HEIGHT);
+        setVelocity(INITIAL_VELOCITY);
         ai = new GoombaAI(this);
     }
 
