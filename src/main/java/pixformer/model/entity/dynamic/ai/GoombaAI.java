621346c16484ee9be13f97b87777a26788857855
Luca Patrignani <luca.patrignani3@studio.unibo.it>
2023-03-08 16:52:11 +0100

    Make GoombaAI use HorizontalModelInput instead of directly accessing Entity

--- src/main/java/pixformer/model/entity/dynamic/ai/GoombaAI.java
+++ src/main/java/pixformer/model/entity/dynamic/ai/GoombaAI.java
@@ -1,12 +1,11 @@
 package pixformer.model.entity.dynamic.ai;
 
-import java.util.Optional;
-
 import pixformer.model.World;
 import pixformer.model.entity.Entity;
 import pixformer.model.entity.collision.EntityCollisionManager;
 import pixformer.model.entity.statics.Block;
 import pixformer.model.input.AIInputComponent;
+import pixformer.model.modelinput.HorizontalModelInput;
 import pixformer.model.entity.collision.Collision;
 
 /**
@@ -14,25 +13,36 @@ import pixformer.model.entity.collision.Collision;
  */
 public class GoombaAI extends AIInputComponent {
 
+    private final HorizontalModelInput joystick;
+
     /**
      * Create a new GoombaAI.
      * 
      * @param entity which this AI will control.
      */
-    public GoombaAI(final Entity entity) {
+    public GoombaAI(final Entity entity, final HorizontalModelInput modelInput) {
         super(entity);
+        this.joystick = modelInput;
     }
 
     @Override
     public final void update(final World world) {
+        System.out.println(world.getEntities());
         final EntityCollisionManager collisionManager = world.getCollisionManager();
         final Entity goomba = super.getEntity();
-        final Optional<?> collidingBlock = collisionManager.findCollisionsFor(goomba).stream()
-                .map(Collision::entity)
-                .filter(Block.class::isInstance)
-                .findFirst();
-        if (collidingBlock.isPresent()) {
-            goomba.setVelocity(goomba.getVelocity().scale(-1));
-        }
+        collisionManager.findCollisionsFor(goomba).stream()
+                .peek(System.out::println)
+                .filter(c -> c.entity() instanceof Block)
+                .findFirst()
+                .map(Collision::side)
+                .ifPresent(c -> {
+                    switch (c) {
+                        case LEFT:
+                            joystick.right(); break;
+                        case RIGHT:
+                            joystick.left(); break;
+                        default: break;
+                    }
+                });
     }
 }
