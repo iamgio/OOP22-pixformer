d591eec5f12d9bfcbdbdad20eb8342e02e4596a8
Luca Patrignani <luca.patrignani3@studio.unibo.it>
2023-03-08 23:13:23 +0100

    Refactor test

--- src/test/java/pixformer/GoombaAITest.java
+++ src/test/java/pixformer/GoombaAITest.java
@@ -2,6 +2,9 @@ package pixformer;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.function.DoublePredicate;
+import java.util.function.DoubleUnaryOperator;
+import java.util.stream.Stream;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
@@ -17,7 +20,7 @@ final class GoombaAITest {
     private static final double DELTA = 0.0001;
     private final World world = new WorldImpl();
     private Goomba goomba;
-    private final double step = -0.002;
+    private final double step = 0.002;
     private final double dt = 1;
 
     @BeforeEach
@@ -29,23 +32,42 @@ final class GoombaAITest {
     @Test
     void testClearRoad() {
         world.update(dt);
-        assertEquals(step, goomba.getX());
+        assertEquals(-step, goomba.getX());
         assertEquals(0, goomba.getY());
         world.update(dt);
-        assertEquals(2 * step, goomba.getX());
+        assertEquals(-2 * step, goomba.getX());
         assertEquals(0, goomba.getY());
     }
 
     @Test
     void testFindObstacle() {
-        world.spawnEntity(new Block(X_BLOCK, 0)); // x_y
-        for (double i = 0; i > -1; i += step) {
-            assertEquals(i, goomba.getX());
+        world.spawnEntity(new Block(X_BLOCK, 0)); // -2, -1
+        world.spawnEntity(new Block(2, 0)); // 1, 2
+        goUntil(0, i -> i > -1, i -> i - step, -1 + step);
+        goUntil(-1 + step, i -> i < 1, i -> i + step, 1 - step);
+        // for (double i = -1 + step; i < 1; i += step) {
+        //     assertEquals(i, goomba.getX(), DELTA);
+        //     assertEquals(0, goomba.getY());
+        //     world.update(dt);
+        // }
+        // world.update(dt);
+        // assertEquals(1 - step, goomba.getX(), DELTA);
+        // assertEquals(0, goomba.getY());
+    }
+
+    private void goUntil(
+            final double start, 
+            final DoublePredicate hasNext, 
+            final DoubleUnaryOperator next,
+            final double finalPosition
+    ) {
+        for (double i = start; hasNext.test(i); i = next.applyAsDouble(i)) {
+            assertEquals(i, goomba.getX(), DELTA);
             assertEquals(0, goomba.getY());
             world.update(dt);
         }
         world.update(dt);
-        assertEquals(-1 - step, goomba.getX(), DELTA);
+        assertEquals(finalPosition, goomba.getX(), DELTA);
         assertEquals(0, goomba.getY());
     }
 }
