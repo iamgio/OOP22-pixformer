22d9bb022ce894567f9d721ce33c24ebff70c43f
Giorgio Garofalo <giogar2705@gmail.com>
2023-03-08 19:58:31 +0100

    Make World immutable

--- src/main/java/pixformer/model/World.java
+++ src/main/java/pixformer/model/World.java
@@ -3,10 +3,7 @@ package pixformer.model;
 import pixformer.common.Updatable;
 import pixformer.model.entity.Entity;
 import pixformer.model.entity.collision.EntityCollisionManager;
-import pixformer.model.entity.dynamic.Player;
 
-import java.util.List;
-import java.util.Optional;
 import java.util.Set;
 
 /**
@@ -30,34 +27,4 @@ public interface World extends Updatable {
      * @return the handler of entity collisions for this world
      */
     EntityCollisionManager getCollisionManager();
-
-    /**
-     * @return immutable list of the active players that may control entities
-     */
-    List<Player> getPlayers();
-
-    /**
-     * Adds a new player to the world.
-     * 
-     * @param player player to add
-     */
-    void addPlayer(Player player);
-
-    /**
-     * @param index index of the player, starting from {@code 0}
-     * @return player at the given index if it exists, or an empty {@link Optional}
-     *         otherwise
-     */
-    default Optional<Player> getPlayer(final int index) {
-        return index < this.getPlayers().size()
-                ? Optional.of(this.getPlayers().get(index))
-                : Optional.empty();
-    }
-
-    /**
-     * @return the first player if it exists
-     */
-    default Optional<Player> getPlayer1() {
-        return this.getPlayer(0);
-    }
 }
--- src/main/java/pixformer/model/WorldImpl.java
+++ src/main/java/pixformer/model/WorldImpl.java
@@ -3,12 +3,9 @@ package pixformer.model;
 import pixformer.model.entity.Entity;
 import pixformer.model.entity.collision.EntityCollisionManager;
 import pixformer.model.entity.collision.EntityCollisionManagerImpl;
-import pixformer.model.entity.dynamic.Player;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 /**
@@ -18,7 +15,6 @@ public class WorldImpl implements World {
 
     private final Set<Entity> entities;
     private final EntityCollisionManager collisionManager;
-    private final List<Player> players = new ArrayList<>();
 
     /**
      * Create a new World.
@@ -51,22 +47,6 @@ public class WorldImpl implements World {
     @Override
     public EntityCollisionManager getCollisionManager() {
         return this.collisionManager;
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public List<Player> getPlayers() {
-        return Collections.unmodifiableList(this.players);
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public void addPlayer(final Player player) {
-        this.players.add(player);
     }
 
     /**
