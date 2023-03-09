c4da3eb017144e1b27e7fc97e920d40f472cb37f
IGieckI <giacomo.antonelli3@gmail.com>
2023-03-09 15:02:44 +0100

    Add PlayerGraphicsComponent and PlayerInputComponent classes

---
+++ src/main/java/pixformer/model/entity/dynamic/player/PlayerInputComponent.java
@@ -0,0 +1,13 @@
+package pixformer.model.entity.dynamic.player;
+
+import pixformer.model.entity.Entity;
+import  pixformer.model.input.InputComponent;
+
+public class PlayerInputComponent extends InputComponent {
+
+    protected PlayerInputComponent(Entity entity) {
+        super(entity);
+        //TODO Auto-generated constructor stub
+    }
+    
+}
