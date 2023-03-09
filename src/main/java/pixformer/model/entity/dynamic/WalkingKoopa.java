2b7e006ddfd1b74b5978c3d5942977a2b0c4849c
Luca Patrignani <luca.patrignani3@studio.unibo.it>
2023-03-09 18:19:08 +0100

    Add docs

--- src/main/java/pixformer/model/entity/dynamic/WalkingKoopa.java
+++ src/main/java/pixformer/model/entity/dynamic/WalkingKoopa.java
@@ -1,12 +1,19 @@
 package pixformer.model.entity.dynamic;
 
-
+/**
+ * The state of the koopa which walks normally and behaves like a goomba.
+ */
 public class WalkingKoopa extends Enemy implements Koopa {
 
     private static final double INITIAL_VELOCITY = 0.002; // calcoli fatti a mano
     private static double WIDTH = 1;
     private static double HEIGHT = 2;
     
+    /**
+     * Construct a WalkingKoopa.
+     * @param x its initial x position
+     * @param y its initial x position.
+     */
     public WalkingKoopa(final double x, final double y) {
         super(x, y, WIDTH, HEIGHT, INITIAL_VELOCITY);
     }
