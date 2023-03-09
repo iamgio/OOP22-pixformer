d69f3c64f2689986813759e3724e274bb007dbad
Luca Patrignani <luca.patrignani3@studio.unibo.it>
2023-03-09 18:20:18 +0100

    Add docs

--- src/main/java/pixformer/model/entity/dynamic/TurtleKoopa.java
+++ src/main/java/pixformer/model/entity/dynamic/TurtleKoopa.java
@@ -1,10 +1,18 @@
 package pixformer.model.entity.dynamic;
 
+/**
+ * The state of the koopa when it is a turtle.
+ */
 public class TurtleKoopa extends Enemy implements Koopa {
 
     private static final double WIDTH = 1;
     private static final double HEIGHT = 1;
 
+    /**
+     * Create a new TurtleKoopa
+     * @param x its initial x position
+     * @param y its initial y position.
+     */
     public TurtleKoopa(final double x, final double y) {
         super(x, y, WIDTH, HEIGHT, 1);
     }
