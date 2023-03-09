c2a3a1226ebfb4694bbe44e15e66203d96ffc877
Luca Patrignani <luca.patrignani3@studio.unibo.it>
2023-03-09 18:03:29 +0100

    Create the collision-reaction system

--- src/main/java/pixformer/model/entity/dynamic/ai/GoombaAI.java
+++ src/main/java/pixformer/model/entity/dynamic/ai/GoombaAI.java
@@ -1,12 +1,19 @@
 package pixformer.model.entity.dynamic.ai;
 
+import java.util.Map;
+import java.util.Set;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+
 import pixformer.model.World;
 import pixformer.model.entity.Entity;
 import pixformer.model.entity.collision.EntityCollisionManager;
+import pixformer.model.entity.dynamic.player.Player;
 import pixformer.model.entity.statics.Block;
 import pixformer.model.input.AIInputComponent;
 import pixformer.model.modelinput.HorizontalModelInput;
 import pixformer.model.entity.collision.Collision;
+import pixformer.model.entity.collision.CollisionSide;
 
 /**
  * A general AI which makes the entity go left until death or it gets into a
@@ -30,21 +37,34 @@ public class GoombaAI extends AIInputComponent {
     public final void update(final World world) {
         final EntityCollisionManager collisionManager = world.getCollisionManager();
         final Entity goomba = super.getEntity();
-        collisionManager.findCollisionsFor(goomba).stream()
-                .filter(c -> c.entity() instanceof Block)
-                .findFirst()
-                .map(Collision::side)
-                .ifPresent(c -> {
-                    switch (c) {
-                        case LEFT:
-                            joystick.left();
-                            break;
-                        case RIGHT:
-                            joystick.right();
-                            break;
-                        default:
-                            break;
-                    }
-                });
+        forCollisions(collisionManager.findCollisionsFor(goomba), Map.of(
+            Block.class::isInstance, this::reactOnBlockCollision,
+            Player.class::isInstance, (side) -> {/* TODO: this entity dies */ }
+        ));
+    }       
+
+    private void forCollisions(
+        final Set<Collision> collisions, 
+        final Map<Predicate<Entity>, Consumer<CollisionSide>> reactions
+    ) {
+            for (final var reaction : reactions.entrySet()) {
+                collisions.stream()
+                    .filter(c -> reaction.getKey().test(c.entity()))
+                    .map(Collision::side)
+                    .forEach(reaction.getValue());
+            }
+    }
+
+    private void reactOnBlockCollision(final CollisionSide side) {
+        switch (side) {
+            case LEFT:
+                joystick.left();
+                break;
+            case RIGHT:
+                joystick.right();
+                break;
+            default:
+                break;
+        }
     }
 }
