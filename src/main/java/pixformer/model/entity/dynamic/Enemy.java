00b1dec4f551fe877fb85887bc7a3be9e2245ca7
Luca Patrignani <luca.patrignani3@studio.unibo.it>
2023-03-09 18:04:18 +0100

    Extract common feature of the enemies creating Enemy class

---
+++ src/main/java/pixformer/model/entity/dynamic/Enemy.java
@@ -0,0 +1,45 @@
+package pixformer.model.entity.dynamic;
+
+import java.util.Optional;
+
+import pixformer.common.Vector2D;
+import pixformer.model.entity.AbstractEntity;
+import pixformer.model.entity.collision.DefaultRectangleBoundingBoxEntity;
+import pixformer.model.entity.dynamic.ai.GoombaAI;
+import pixformer.model.input.InputComponent;
+import pixformer.model.modelinput.HorizontalModelInput;
+
+/**
+ * The default implementation of an enemy.
+ */
+public class Enemy extends AbstractEntity implements DefaultRectangleBoundingBoxEntity {
+
+    private static final double INITIAL_VELOCITY = 0.002; // calcoli fatti a mano
+    private final HorizontalModelInput joystick;
+    private final GoombaAI ai;
+
+    /**
+     * Create a Goomba at position ({@code x}, {@code y}).
+     * 
+     * @param x coordinate
+     * @param y coordinate.
+     */
+    public Enemy(final double x, final double y, final double width, final double height) {
+        super(x, y, width, height);
+        joystick = new HorizontalModelInputImpl(this::fixVelocity, INITIAL_VELOCITY);
+        ai = new GoombaAI(this, joystick);
+        joystick.left();
+    }
+
+    @Override
+    public final Optional<InputComponent> getInputComponent() {
+        return Optional.of(ai);
+    }
+
+    private void fixVelocity(final Vector2D velocity) {
+        final Vector2D current = super.getVelocity();
+        final Vector2D newVelocity = new Vector2D(velocity.x(), current.y());
+        this.setVelocity(newVelocity);
+    }
+
+}
