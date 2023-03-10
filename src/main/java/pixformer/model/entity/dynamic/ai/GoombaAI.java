4ba91645715d693af8e6f4d069f996e78854f5dc
Luca Patrignani <luca.patrignani3@studio.unibo.it>
2023-03-10 15:39:34 +0100

    Make GoombaAI use CollisionReactor

--- src/main/java/pixformer/model/entity/dynamic/ai/GoombaAI.java
+++ src/main/java/pixformer/model/entity/dynamic/ai/GoombaAI.java
@@ -9,7 +9,9 @@ import pixformer.model.World;
 import pixformer.model.entity.AbstractEntity;
 import pixformer.model.entity.Entity;
 import pixformer.model.entity.collision.Collision;
+import pixformer.model.entity.collision.CollisionReactor;
 import pixformer.model.entity.collision.EntityCollisionManager;
+import pixformer.model.entity.collision.SimpleCollisionReactor;
 import pixformer.model.entity.dynamic.player.Player;
 import pixformer.model.entity.statics.Block;
 import pixformer.model.input.AIInputComponent;
@@ -23,6 +25,7 @@ import pixformer.model.entity.collision.CollisionSide;
 public class GoombaAI extends AIInputComponent {
 
     private final HorizontalModelInput joystick;
+    private final CollisionReactor reactor;
 
     /**
      * Create a new GoombaAI.
@@ -32,29 +35,18 @@ public class GoombaAI extends AIInputComponent {
     public GoombaAI(final AbstractEntity entity, final HorizontalModelInput modelInput) {
         super(entity);
         this.joystick = modelInput;
+        reactor = new SimpleCollisionReactor(Map.of(
+            Block.class::isInstance, this::reactOnBlockCollision,
+            Player.class::isInstance, (side) -> {/* TODO: this entity dies */ }
+        ));
     }
 
     @Override
     public final void update(final World world) {
         final EntityCollisionManager collisionManager = world.getCollisionManager();
         final Entity goomba = super.getEntity();
-        forCollisions(collisionManager.findCollisionsFor(goomba), Map.of(
-            Block.class::isInstance, this::reactOnBlockCollision,
-            Player.class::isInstance, (side) -> {/* TODO: this entity dies */ }
-        ));
+        reactor.react(collisionManager.findCollisionsFor(goomba));
     }       
-
-    private void forCollisions(
-        final Set<Collision> collisions, 
-        final Map<Predicate<Entity>, Consumer<CollisionSide>> reactions
-    ) {
-            for (final var reaction : reactions.entrySet()) {
-                collisions.stream()
-                    .filter(c -> reaction.getKey().test(c.entity()))
-                    .map(Collision::side)
-                    .forEach(reaction.getValue());
-            }
-    }
 
     private void reactOnBlockCollision(final CollisionSide side) {
         switch (side) {
