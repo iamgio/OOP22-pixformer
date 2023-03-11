c8b1368644c6a41062c8b49b2e17b92295dbde20
NickGhignatti <nick.ghignatti@gmail.com>
2023-03-11 20:26:34 +0100

    Static entitis are now solid

--- src/main/java/pixformer/model/entity/statics/Brick.java
+++ src/main/java/pixformer/model/entity/statics/Brick.java
@@ -1,15 +1,20 @@
 package pixformer.model.entity.statics;
 
 import pixformer.model.entity.AbstractEntity;
+import pixformer.model.entity.DrawableEntity;
+import pixformer.model.entity.GraphicsComponent;
 import pixformer.model.entity.collision.DefaultRectangleBoundingBoxEntity;
+import pixformer.model.entity.collision.SolidEntity;
 
 /**
  * Brick block, common block which can be destroyed.
  */
-public class Brick extends AbstractEntity implements DefaultRectangleBoundingBoxEntity {
+public class Brick extends AbstractEntity implements DefaultRectangleBoundingBoxEntity, SolidEntity, DrawableEntity {
 
     private static final double WIDTH = 1;
     private static final double HEIGHT = 1;
+
+    private final GraphicsComponent graphicsComponent = null;
 
     /**
      * Constructor of the Block.
@@ -21,4 +26,11 @@ public class Brick extends AbstractEntity implements DefaultRectangleBoundingBox
         super(x, y, WIDTH, HEIGHT);
     }
 
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public GraphicsComponent getGraphicsComponent() {
+        return this.graphicsComponent;
+    }
 }
--- src/main/java/pixformer/model/entity/statics/Surprise.java
+++ src/main/java/pixformer/model/entity/statics/Surprise.java
@@ -1,15 +1,20 @@
 package pixformer.model.entity.statics;
 
 import pixformer.model.entity.AbstractEntity;
+import pixformer.model.entity.DrawableEntity;
+import pixformer.model.entity.GraphicsComponent;
 import pixformer.model.entity.collision.DefaultRectangleBoundingBoxEntity;
+import pixformer.model.entity.collision.SolidEntity;
 
 /**
  * Surprise block, which contains a power-up.
  */
-public class Surprise extends AbstractEntity implements DefaultRectangleBoundingBoxEntity {
+public class Surprise extends AbstractEntity implements DefaultRectangleBoundingBoxEntity, SolidEntity, DrawableEntity {
 
     private static final double WIDTH = 1;
     private static final double HEIGHT = 1;
+
+    private final GraphicsComponent graphicsComponent = null;
 
     /**
      * Constructor of the Surprise block.
@@ -21,4 +26,11 @@ public class Surprise extends AbstractEntity implements DefaultRectangleBounding
         super(x, y, WIDTH, HEIGHT);
     }
 
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public GraphicsComponent getGraphicsComponent() {
+        return this.graphicsComponent;
+    }
 }
