98ac402899f2465810529bec0788dd4c6f42f006
IGieckI <giacomo.antonelli3@gmail.com>
2023-03-11 17:04:09 +0100

    Update to PlayerGrahicsComponent class

--- src/main/java/pixformer/model/entity/dynamic/player/PlayerGraphicsComponent.java
+++ src/main/java/pixformer/model/entity/dynamic/player/PlayerGraphicsComponent.java
@@ -1,19 +1,23 @@
 package pixformer.model.entity.dynamic.player;
 
+import pixformer.model.entity.Entity;
 import pixformer.model.entity.GraphicsComponent;
 import pixformer.view.engine.GameScene;
 
 /**
  * Implementation of GraphicComponent for a Player entity.
  */
-public class PlayerGraphicsComponent implements GraphicsComponent {
+public class PlayerGraphicsComponent extends GraphicsComponent {
 
-    /**
-     * {@inheritDoc}
-     */
+    protected PlayerGraphicsComponent(Entity entity) {
+        super(entity);
+        //TODO Auto-generated constructor stub
+    }
+
     @Override
-    public void update(final GameScene scene) {
+    public void update(GameScene scene) {
         // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'update'");
     }
 
 }
