73fafb505451eebc49b10b3eb09871f384907c2e
Giorgio Garofalo <giogar2705@gmail.com>
2023-03-12 19:08:35 +0100

    Improve level deserialization test

--- src/test/java/pixformer/LevelDeserializationTest.java
+++ src/test/java/pixformer/LevelDeserializationTest.java
@@ -3,9 +3,12 @@ package pixformer;
 import org.junit.jupiter.api.Test;
 import pixformer.controller.deserialization.level.JsonLevelDataDeserializer;
 import pixformer.model.LevelData;
+import pixformer.model.entity.Entity;
 import pixformer.model.entity.EntityFactoryImpl;
+import pixformer.model.entity.statics.Block;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertInstanceOf;
 
 /**
  * Test of level deserialization.
@@ -17,12 +20,15 @@ public class LevelDeserializationTest {
         final LevelData data = new JsonLevelDataDeserializer(new EntityFactoryImpl())
                 .deserialize(this.getClass().getResourceAsStream("/levels/test1.json"));
 
-        System.out.println(data);
-        System.out.println(data.entities().iterator().next().getX());
-
         assertEquals("First level", data.name());
         assertEquals(1, data.spawnPointX());
         assertEquals(2, data.spawnPointY());
         assertEquals(1, data.entities().size());
+
+        Entity first = data.entities().iterator().next();
+
+        assertInstanceOf(Block.class, first);
+        assertEquals(first.getX(), 10);
+        assertEquals(first.getY(), 10);
     }
 }
