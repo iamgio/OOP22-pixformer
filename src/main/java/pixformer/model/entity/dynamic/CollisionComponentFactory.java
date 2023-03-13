2fec7d8908f9dbdf2a7ed9bfe5229902e724ac80
Luca Patrignani <luca.patrignani3@studio.unibo.it>
2023-03-13 17:24:29 +0100

    Make CollisionComponentFactory a utility class and add docs to it

--- src/main/java/pixformer/model/entity/dynamic/CollisionComponentFactory.java
+++ src/main/java/pixformer/model/entity/dynamic/CollisionComponentFactory.java
@@ -6,8 +6,21 @@ import pixformer.model.entity.collision.Collision;
 import pixformer.model.entity.collision.CollisionComponent;
 import pixformer.model.entity.collision.CollisionReactor;
 
-public class CollisionComponentFactory {
+/**
+ * Utility static class for transforming a CollsionReactor to a
+ * CollisionComponent.
+ */
+public final class CollisionComponentFactory {
 
+    private CollisionComponentFactory() {
+        
+    }
+
+    /**
+     * @param collisionReactor whose behaviour will be replicated by the resulting
+     *                         CollisionComponent.
+     * @return a CollisionComponent which behaves like the {@code CollisionReactor}.
+     */
     public static CollisionComponent fromReactor(final CollisionReactor collisionReactor) {
         return new CollisionComponent(null) {
 
