084d46e90513699674f4a05ab76721f386445740
Luca Patrignani <luca.patrignani3@studio.unibo.it>
2023-03-13 15:44:42 +0100

    Create a factory for CollisionComponent which convert a reactor to a CollisionComponent

---
+++ src/main/java/pixformer/model/entity/dynamic/CollisionComponentFactory.java
@@ -0,0 +1,21 @@
+package pixformer.model.entity.dynamic;
+
+import java.util.Set;
+
+import pixformer.model.entity.collision.Collision;
+import pixformer.model.entity.collision.CollisionComponent;
+import pixformer.model.entity.collision.CollisionReactor;
+
+public class CollisionComponentFactory {
+
+    public static CollisionComponent fromReactor(final CollisionReactor collisionReactor) {
+        return new CollisionComponent(null) {
+
+            @Override
+            public void update(final double dt, final Set<Collision> collisions) {
+                collisionReactor.react(collisions);
+            }
+
+        };
+    }
+}
