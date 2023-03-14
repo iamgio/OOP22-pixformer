3c6fe5ad93ec779688f24cd7e97aa4f5da457355
NickGhignatti <nick.ghignatti@gmail.com>
2023-03-14 17:01:09 +0100

    Add events management

---
+++ src/main/java/pixformer/model/event/EventManager.java
@@ -0,0 +1,26 @@
+package pixformer.model.event;
+
+import pixformer.model.entity.Entity;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.function.Consumer;
+
+public class EventManager implements EventSubscriber, EventHandler{
+
+    private final Set<Consumer<Entity>> listeners;
+
+    public EventManager() {
+        this.listeners = new HashSet<>();
+    }
+
+    @Override
+    public void die(final Entity entity) {
+        listeners.forEach(cons -> cons.accept(entity));
+    }
+
+    @Override
+    public void addPlayerOnKill(final Consumer<Entity> consumer) {
+        this.listeners.add(consumer);
+    }
+}
