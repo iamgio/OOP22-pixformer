b9e24cbf7ba5243738103f9476e318f374bddfb3
IGieckI <giacomo.antonelli3@gmail.com>
2023-03-14 18:22:11 +0100

    Add collision management within Player for Goombas and Koopas

--- src/main/java/pixformer/model/entity/dynamic/player/PlayerCollisionComponent.java
+++ src/main/java/pixformer/model/entity/dynamic/player/PlayerCollisionComponent.java
@@ -1,8 +1,13 @@
 package pixformer.model.entity.dynamic.player;
 
 import java.util.Set;
+
+import javafx.geometry.Side;
 import pixformer.model.entity.collision.Collision;
 import pixformer.model.entity.collision.CollisionComponent;
+import pixformer.model.entity.collision.CollisionSide;
+import pixformer.model.entity.dynamic.Goomba;
+import pixformer.model.entity.dynamic.Koopa;
 
 /**
  * Implementation of CollisionComponent for a Player entity.
@@ -30,7 +35,11 @@ public class PlayerCollisionComponent extends CollisionComponent {
         }
 
         for (var x : collisions) {
-            throw new UnsupportedOperationException("Unimplemented method 'update'");
+            if(x.entity().getClass().equals(Goomba.class) || x.entity().getClass().equals(Koopa.class)) {
+                if(x.side() == CollisionSide.LEFT || x.side() == CollisionSide.RIGHT) {
+                    this.player.getDamage();
+                }
+            }
         }
 
     }
