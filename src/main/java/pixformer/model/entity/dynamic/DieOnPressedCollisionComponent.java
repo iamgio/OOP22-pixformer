7b4c51677f2185eaba09d5d47b2eb27e18258454
Luca Patrignani <luca.patrignani3@studio.unibo.it>
2023-03-15 22:59:15 +0100

    Add docs

--- src/main/java/pixformer/model/entity/dynamic/DieOnPressedCollisionComponent.java
+++ src/main/java/pixformer/model/entity/dynamic/DieOnPressedCollisionComponent.java
@@ -1,4 +1,5 @@
 package pixformer.model.entity.dynamic;
+
 import java.util.Map;
 import java.util.Set;
 import java.util.function.Consumer;
@@ -12,20 +13,27 @@ import pixformer.model.entity.collision.CollisionSide;
 import pixformer.model.entity.collision.SimpleCollisionReactor;
 import pixformer.model.entity.dynamic.player.Player;
 
+/**
+ * A collision component which makes its entity die when it collides from the
+ * top with a Player.
+ */
 public final class DieOnPressedCollisionComponent extends CollisionComponent {
 
     private final CollisionReactor reactor;
 
-    protected DieOnPressedCollisionComponent(final MutableEntity entity, final Consumer<Entity> die) {
+    /**
+     * @param entity to be controlled.
+     * @param die    the consumer to which will be passed this entity to kill it.
+     */
+    public DieOnPressedCollisionComponent(final MutableEntity entity, final Consumer<Entity> die) {
         super(entity);
         reactor = new SimpleCollisionReactor(Map.of(
-            Player.class::isInstance, new SideCheckerConsumer(CollisionSide.TOP, () -> die.accept(entity))
-        ));
+                Player.class::isInstance, new SideCheckerConsumer(CollisionSide.TOP, () -> die.accept(entity))));
     }
 
     @Override
     public void update(final double dt, final Set<Collision> collisions) {
         reactor.react(collisions);
     }
-    
+
 }
