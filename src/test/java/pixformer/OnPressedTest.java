0c9feb06259b21de2939f544513857c511198947
Luca Patrignani <luca.patrignani3@studio.unibo.it>
2023-03-16 00:05:34 +0100

    Rename test and test class

--- src/test/java/pixformer/OnPressedTest.java
+++ src/test/java/pixformer/OnPressedTest.java
@@ -13,31 +13,31 @@ import pixformer.model.entity.dynamic.player.Player;
 import pixformer.model.entity.statics.Block;
 
 /**
- * Tests to check if the {@link pixformer.model.entity.dynamic.DieOnPressedCollisionComponent} works.
+ * Tests to check if the
+ * {@link pixformer.model.entity.dynamic.DieOnPressedCollisionComponent} works.
  */
-public class DieGoombaTest {
+public class OnPressedTest {
 
     private static final int SECONDS_TO_MILLIS = 1_000;
     private static final int FPS = 30;
     private static final double DT = SECONDS_TO_MILLIS / FPS * 30;
 
     @Test
-    void testIfDies() {
+    void testIfGoombaDies() {
         final World world = new WorldImpl();
         /*
-        *  p
-        *  p
-        * #g#
-        *  #
-        */
+         * p
+         * p
+         * #g#
+         * #
+         */
         final Goomba goomba = new Goomba(0, 2);
         Set.of(
-            goomba,
-            new Player(0, 0, 1, 2, 0),
-            new Block(-1, 2),
-            new Block(1, 2),
-            new Block(0, 3)
-        ).forEach(world::spawnEntity);
+                goomba,
+                new Player(0, 0, 1, 2, 0),
+                new Block(-1, 2),
+                new Block(1, 2),
+                new Block(0, 3)).forEach(world::spawnEntity);
         world.update(DT);
         assertTrue(!world.getEntities().contains(goomba));
     }
