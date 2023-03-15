36ac79b57a71e388a8baf1832933daadcf87575a
Luca Patrignani <luca.patrignani3@studio.unibo.it>
2023-03-15 18:57:18 +0100

    Create SideCheckerConsumer

---
+++ src/main/java/pixformer/model/entity/dynamic/SideCheckerConsumer.java
@@ -0,0 +1,31 @@
+package pixformer.model.entity.dynamic;
+
+import java.util.function.Consumer;
+
+import pixformer.model.entity.collision.CollisionSide;
+
+/**
+ * A Consumer which runs only if the CollisionSide passed to it respect some conditions.
+ */
+public class SideCheckerConsumer implements Consumer<CollisionSide> {
+
+    private final CollisionSide checkingSide;
+    private final Runnable runnable;
+
+    /**
+     * @param checkingSide the only accepted side.
+     * @param runnable to be run is the condition is satisfied.
+     */
+    public SideCheckerConsumer(final CollisionSide checkingSide, final Runnable runnable) {
+        this.checkingSide = checkingSide;
+        this.runnable = runnable;
+    }
+
+    @Override
+    public void accept(final CollisionSide arg0) {
+        if (arg0 == checkingSide) {
+            runnable.run();
+        }
+    }
+
+}
