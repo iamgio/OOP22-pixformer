8db1d62b39a4bc735846dcec8fc5cff8a3806227
IGieckI <giacomo.antonelli3@gmail.com>
2023-03-15 17:45:37 +0100

    Implement of FIreballPhysicsComponent

--- src/main/java/pixformer/model/entity/powerups/other/Fireball/FireballPhysicsComponent.java
+++ src/main/java/pixformer/model/entity/powerups/other/Fireball/FireballPhysicsComponent.java
@@ -1,18 +1,30 @@
 package pixformer.model.entity.powerups.other.Fireball;
 
-import pixformer.model.entity.AbstractEntity;
+import pixformer.common.Vector2D;
 import pixformer.model.physics.PhysicsComponent;
 
 /**
  * Implementation of PhysicalComponent for a Player entity.
  */
 public class FireballPhysicsComponent extends PhysicsComponent {
+    private Fireball fireball;
 
     /**
      * 
      * @param entity Entity affected by the class physics rules.
      */
-    public FireballPhysicsComponent(final AbstractEntity entity) {
-        super(entity);
+    public FireballPhysicsComponent(final Fireball fireball) {
+        super(fireball);
+        this.fireball = fireball;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void update(final double dt) {
+        super.update(dt);
+
+        fireball.setVelocity(new Vector2D(fireball.getSpeed(), fireball.getVelocity().y()));
     }
 }
