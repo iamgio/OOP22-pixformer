f9b948ee7391ae970c361d909a51015c861c7cb8
NickGhignatti <nick.ghignatti@gmail.com>
2023-03-15 17:32:33 +0100

    Add a collision component for the brick, now it is destroyable

--- src/main/java/pixformer/model/entity/statics/Brick.java
+++ src/main/java/pixformer/model/entity/statics/Brick.java
@@ -1,14 +1,17 @@
 package pixformer.model.entity.statics;
 
-import com.sun.javafx.reflect.ConstructorUtil;
 import pixformer.model.entity.AbstractEntity;
 import pixformer.model.entity.DrawableEntity;
-import pixformer.model.entity.Entity;
 import pixformer.model.entity.GraphicsComponent;
+import pixformer.model.entity.collision.Collision;
+import pixformer.model.entity.collision.CollisionComponent;
 import pixformer.model.entity.collision.DefaultRectangleBoundingBoxEntity;
 import pixformer.model.entity.collision.SolidEntity;
+import pixformer.view.engine.Color;
+import pixformer.view.entity.RectangleGraphicsComponent;
 
-import java.util.function.Consumer;
+import java.util.Optional;
+import java.util.Set;
 
 /**
  * Brick block, common block which can be destroyed.
@@ -17,8 +20,7 @@ public class Brick extends AbstractEntity implements DefaultRectangleBoundingBox
 
     private static final double WIDTH = 1;
     private static final double HEIGHT = 1;
-
-    private final GraphicsComponent graphicsComponent = null;
+    private final GraphicsComponent graphicsComponent = new RectangleGraphicsComponent(this, new Color(0, 0, 1));
 
     /**
      * Constructor of the Block.
@@ -36,5 +38,26 @@ public class Brick extends AbstractEntity implements DefaultRectangleBoundingBox
     @Override
     public GraphicsComponent getGraphicsComponent() {
         return this.graphicsComponent;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Optional<CollisionComponent> getCollisionComponent() {
+        return Optional.of(new CollisionComponent(this) {
+
+            /**
+             * {@inheritDoc}
+             */
+            @Override
+            public void update(double dt, Set<Collision> collisions) {
+                for (var collision : collisions) {
+                    if (collision.side().isVertical() && getWorld().isPresent()) {
+                        getWorld().get().killEntity(this.getEntity());
+                    }
+                }
+            }
+        });
     }
 }
