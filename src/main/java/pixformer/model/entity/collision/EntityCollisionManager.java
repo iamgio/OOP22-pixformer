63469d119c85452dd55479e648d37d17ee636e54
Giorgio Garofalo <giogar2705@gmail.com>
2023-03-16 18:39:31 +0100

    Add EntityCollisionManager solid entity collision detection

--- src/main/java/pixformer/model/entity/collision/EntityCollisionManager.java
+++ src/main/java/pixformer/model/entity/collision/EntityCollisionManager.java
@@ -15,4 +15,28 @@ public interface EntityCollisionManager {
      * @return the group of collisions involving the given entity
      */
     Set<Collision> findCollisionsFor(Entity entity);
+
+    /**
+     * @param entity entity to check collisions for
+     * @return whether the entity is colliding a solid entity on the bottom side.
+     */
+    boolean isCollidingGround(Entity entity);
+
+    /**
+     * @param entity entity to check collisions for
+     * @return whether the entity is colliding a solid entity on the top side.
+     */
+    boolean isCollidingCeiling(Entity entity);
+
+    /**
+     * @param entity entity to check collisions for
+     * @return whether the entity is colliding a solid entity on the right side.
+     */
+    boolean isCollidingRightWall(Entity entity);
+
+    /**
+     * @param entity entity to check collisions for
+     * @return whether the entity is colliding a solid entity on the left side.
+     */
+    boolean isCollidingLeft(Entity entity);
 }
