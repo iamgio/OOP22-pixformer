6b2084980568c01deef1250d94eddf51e13d47fa
NickGhignatti <nick.ghignatti@gmail.com>
2023-03-17 14:31:35 +0100

    Fix javadoc and checkstyle

--- src/main/java/pixformer/model/event/EventManager.java
+++ src/main/java/pixformer/model/event/EventManager.java
@@ -6,19 +6,31 @@ import java.util.HashSet;
 import java.util.Set;
 import java.util.function.Consumer;
 
+/**
+ * Event manager to implements multiple listeners for the events.
+ */
 public class EventManager implements EventSubscriber, EventHandler{
 
     private final Set<Consumer<Entity>> listeners;
 
+    /**
+     * Constructor for the event manager.
+     */
     public EventManager() {
         this.listeners = new HashSet<>();
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void killed(final Entity entity) {
         listeners.forEach(cons -> cons.accept(entity));
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void addPlayerOnKill(final Consumer<Entity> consumer) {
         this.listeners.add(consumer);
