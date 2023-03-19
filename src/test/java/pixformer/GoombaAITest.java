0b21aa8dae329f3106e4b55332b8de912bd4e263
Luca Patrignani <luca.patrignani3@studio.unibo.it>
2023-03-19 12:09:08 +0100

    Make GoombaAI test just testing the goomba artificial intelligence

--- src/test/java/pixformer/GoombaAITest.java
+++ src/test/java/pixformer/GoombaAITest.java
@@ -2,6 +2,7 @@ package pixformer;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.Optional;
 import java.util.function.DoublePredicate;
 import java.util.function.DoubleUnaryOperator;
 
@@ -10,15 +11,36 @@ import org.junit.jupiter.api.BeforeEach;
 
 import pixformer.model.World;
 import pixformer.model.WorldImpl;
+import pixformer.model.entity.AbstractEntity;
+import pixformer.model.entity.Entity;
+import pixformer.model.entity.collision.BoundingBox;
+import pixformer.model.entity.collision.RectangleBoundingBox;
 import pixformer.model.entity.dynamic.Enemy;
+import pixformer.model.entity.dynamic.ai.GoombaAI;
 import pixformer.model.entity.statics.Block;
+import pixformer.model.input.InputComponent;
 
 final class GoombaAITest {
 
     private static final double X_BLOCK = -2;
     private static final double DELTA = 0.0001;
     private final World world = new WorldImpl();
-    private final Enemy goomba = new Enemy(0, 0, 1, 1, step);
+    // private final Enemy goomba = new Enemy(0, 0, 1, 1, step);
+    private final Entity goomba = new AbstractEntity(0, 0, 1, 1) {
+
+        private final Optional<InputComponent> ai = Optional.of(new GoombaAI(this, this::setVelocity, step));
+
+        @Override
+        public BoundingBox getBoundingBox() {
+            return new RectangleBoundingBox(getWidth(), getHeight());
+        }
+
+        @Override
+        public Optional<InputComponent> getInputComponent() {
+            return ai;
+        }
+        
+    };
     private static final double step = 0.002;
     private static final double dt = 1;
 
@@ -61,11 +83,11 @@ final class GoombaAITest {
             final double finalPosition) {
         for (double i = start; hasNext.test(i); i = next.applyAsDouble(i)) {
             assertEquals(i, goomba.getX(), DELTA);
-            assertEquals(0, goomba.getY());
+            assertEquals(0, goomba.getY(), DELTA);
             world.update(dt);
         }
         world.update(dt);
         assertEquals(finalPosition, goomba.getX(), DELTA);
-        assertEquals(0, goomba.getY());
+        assertEquals(0, goomba.getY(), DELTA);
     }
 }
