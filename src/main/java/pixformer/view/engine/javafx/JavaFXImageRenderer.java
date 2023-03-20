5508e370c6786653ca61b83984577cef175b7820
Giorgio Garofalo <giogar2705@gmail.com>
2023-03-20 18:31:54 +0100

    Add image renderer to renderer factory

--- src/main/java/pixformer/view/engine/javafx/JavaFXImageRenderer.java
+++ src/main/java/pixformer/view/engine/javafx/JavaFXImageRenderer.java
@@ -12,27 +12,20 @@ public class JavaFXImageRenderer extends PositionableRenderer {
     private final Image image;
     private final double width;
     private final double height;
-    private final double sourceX;
-    private final double sourceY;
 
     /**
      * Creates a static image renderer for JavaFX.
      * @param image image to render
      * @param width image width
      * @param height image height
-     * @param sourceX starting X coordinate of the image
-     * @param sourceY starting Y coordinate of the image
      */
     public JavaFXImageRenderer(
             final Image image,
-            final double width, final double height,
-            final double sourceX, final double sourceY
+            final double width, final double height
     ) {
         this.image = image;
         this.width = width;
         this.height = height;
-        this.sourceX = sourceX;
-        this.sourceY = sourceY;
     }
 
     /**
@@ -40,7 +33,7 @@ public class JavaFXImageRenderer extends PositionableRenderer {
      * @param image image to render
      */
     public JavaFXImageRenderer(final Image image) {
-        this(image, image.getWidth(), image.getHeight(), 0D, 0D);
+        this(image, image.getWidth(), image.getHeight());
     }
 
     /**
@@ -49,7 +42,6 @@ public class JavaFXImageRenderer extends PositionableRenderer {
     @Override
     public void render(final double dt, final Graphics graphics) {
         JavaFXGraphics.requireJFXGraphics(graphics).getGraphics().drawImage(image,
-                this.sourceX, this.sourceY, this.width, this.height,
                 super.getX(), super.getY(), this.width, this.height
         );
     }
