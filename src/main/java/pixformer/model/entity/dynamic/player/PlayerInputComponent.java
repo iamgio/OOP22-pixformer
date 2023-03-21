05f5a6af505fce8a39eb99563b7594f2878851a6
IGieckI <giacomo.antonelli3@gmail.com>
2023-03-21 17:43:25 +0100

    VelocitySetterFactory implementation

--- src/main/java/pixformer/model/entity/dynamic/player/PlayerInputComponent.java
+++ src/main/java/pixformer/model/entity/dynamic/player/PlayerInputComponent.java
@@ -122,13 +122,8 @@ public class PlayerInputComponent extends UserInputComponent implements Complete
         jumpKey = false;
 
         // Player speed limit and sprint management
-        final int direction = player.getVelocity().x() >= 0 ? 1 : -1;
 
-        if (!sprintKey && Math.abs(player.getVelocity().x()) > BASE_SPEED_LIMIT) {
-            player.setVelocity(new Vector2D(BASE_SPEED_LIMIT * direction, player.getVelocity().y()));
-        } else if (sprintKey && Math.abs(player.getVelocity().x()) > SPRINT_SPEED_LIMIT) {
-            player.setVelocity(new Vector2D(SPRINT_SPEED_LIMIT * direction, player.getVelocity().y()));
-        }
+        VelocitySetterFactory.limitSpeed(player, sprintKey ? SPRINT_SPEED_LIMIT : BASE_SPEED_LIMIT);
 
         sprintKey = false;
     }
