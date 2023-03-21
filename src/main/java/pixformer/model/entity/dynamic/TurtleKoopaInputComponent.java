739ff5a7c984503427ee9a383c1885c7520995c1
Luca Patrignani <luca.patrignani3@studio.unibo.it>
2023-03-21 18:36:33 +0100

    Make TurtleKoopaInputComponent final and add docs

--- src/main/java/pixformer/model/entity/dynamic/TurtleKoopaInputComponent.java
+++ src/main/java/pixformer/model/entity/dynamic/TurtleKoopaInputComponent.java
@@ -12,17 +12,24 @@ import pixformer.model.input.AIInputComponent;
 import pixformer.model.input.InputComponent;
 import pixformer.model.modelinput.HorizontalModelInput;
 
-public class TurtleKoopaInputComponent extends AIInputComponent {
+/**
+ * The InputComponet of the TurtleKoopa. Makes the entity goes like a goomba but
+ * it starts moving when colliding with a block o a Player.
+ */
+public final class TurtleKoopaInputComponent extends AIInputComponent {
 
     private static final double VELOCITY = 0.02;
     private static final Consumer<HorizontalModelInput> NO_ACTION = (c) -> {};
 
     private final InputComponent wrappedInputComponent;
 
+    /**
+     * @param entity to be controlled.
+     */
     public TurtleKoopaInputComponent(final MutableEntity entity) {
         super(entity);
-        wrappedInputComponent = new GoombaAI(getEntity(), this::velocitySetter, VELOCITY, 
-            e -> e instanceof Block || e instanceof Player, NO_ACTION);
+        wrappedInputComponent = new GoombaAI(getEntity(), this::velocitySetter, VELOCITY,
+                e -> e instanceof Block || e instanceof Player, NO_ACTION);
     }
 
     private void velocitySetter(final Vector2D velocity) {
@@ -33,7 +40,5 @@ public class TurtleKoopaInputComponent extends AIInputComponent {
     public void update(final World world) {
         wrappedInputComponent.update(world);
     }
-
-    
 
 }
