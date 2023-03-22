adfdae508f3d8e0ee4efac87dfdf2bcf305da1e2
Luca Patrignani <luca.patrignani3@studio.unibo.it>
2023-03-22 22:09:23 +0100

    Add graphics and physics components to the powerup entities

--- src/main/java/pixformer/model/entity/dynamic/MovingPowerupEntity.java
+++ src/main/java/pixformer/model/entity/dynamic/MovingPowerupEntity.java
@@ -1,39 +1,44 @@
 package pixformer.model.entity.dynamic;
 
 import pixformer.common.Vector2D;
-import pixformer.model.entity.AbstractEntity;
-import pixformer.model.entity.Entity;
+import pixformer.model.entity.*;
 import pixformer.model.entity.collision.DefaultRectangleBoundingBoxEntity;
+import pixformer.model.entity.collision.SolidEntity;
 import pixformer.model.entity.dynamic.ai.GoombaAI;
 import pixformer.model.entity.powerup.PhysicalPowerup;
 import pixformer.model.entity.powerup.PowerupBehaviour;
 import pixformer.model.input.InputComponent;
 import pixformer.model.modelinput.HorizontalModelInput;
+import pixformer.model.physics.PhysicsComponent;
 
 import java.util.Optional;
 import java.util.function.Consumer;
+import java.util.function.Supplier;
 
 /**
  * Represents a powerUp entity which moves. It moves like a Goomba but its first direction is right.
  */
-public final class MovingPowerupEntity extends AbstractEntity implements PhysicalPowerup, DefaultRectangleBoundingBoxEntity {
+public final class MovingPowerupEntity extends AbstractEntity implements PhysicalPowerup, DefaultRectangleBoundingBoxEntity, DrawableEntity, SolidEntity {
+
 
     private static final double WIDTH = 1;
     private static final double HEIGHT = 1;
-    private static final double VELOCITY = 0.01;
+    private static final double VELOCITY = 0.003;
     private final InputComponent inputComponent;
     private final PowerupBehaviour powerupBehaviour;
+    private final GraphicsComponent graphicsComponent;
 
     /**
      * @param x its initial x position
      * @param y its initial y position
      * @param powerupBehaviour which this entity will represent.
      */
-    public MovingPowerupEntity(final double x, final double y, final PowerupBehaviour powerupBehaviour) {
+    public MovingPowerupEntity(final double x, final double y, final PowerupBehaviour powerupBehaviour, final GraphicsComponentRetriever graphicsComponent) {
         super(x, y, WIDTH, HEIGHT);
         final Consumer<Vector2D> setter = new ProxyMutableEntity(this)::setVelocity;
         inputComponent = new GoombaAI(this, setter, VELOCITY, Entity::isSolid, HorizontalModelInput::right);
         this.powerupBehaviour = powerupBehaviour;
+        this.graphicsComponent = graphicsComponent.apply(this);
     }
 
     @Override
@@ -46,4 +51,12 @@ public final class MovingPowerupEntity extends AbstractEntity implements Physica
         return powerupBehaviour;
     }
 
+    @Override
+    public GraphicsComponent getGraphicsComponent() {
+        return graphicsComponent;
+    }
+    @Override
+    public Optional<PhysicsComponent> getPhysicsComponent() {
+        return Optional.of(new PhysicsComponent(this));
+    }
 }
