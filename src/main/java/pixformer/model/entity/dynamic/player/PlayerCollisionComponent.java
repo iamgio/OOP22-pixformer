5accf35e7ba13dde2e8d64fa084d53754da5c122
IGieckI <giacomo.antonelli3@gmail.com>
2023-03-22 14:43:20 +0100

    Implemented a push-down force while hitting a block vertically

--- src/main/java/pixformer/model/entity/dynamic/player/PlayerCollisionComponent.java
+++ src/main/java/pixformer/model/entity/dynamic/player/PlayerCollisionComponent.java
@@ -2,9 +2,11 @@ package pixformer.model.entity.dynamic.player;
 
 import java.util.Set;
 
+import pixformer.common.Vector2D;
 import pixformer.common.time.ChronometerImpl;
 import pixformer.model.entity.collision.Collision;
 import pixformer.model.entity.collision.CollisionComponent;
+import pixformer.model.entity.collision.CollisionSide;
 import pixformer.model.entity.dynamic.Enemy;
 import pixformer.model.entity.powerup.PhysicalPowerup;
 
@@ -13,6 +15,7 @@ import pixformer.model.entity.powerup.PhysicalPowerup;
  */
 public class PlayerCollisionComponent extends CollisionComponent {
     private static final long INVULNERABILITY_TIME = 3000;
+    private static final double PUSH_DOWN_FORCE = 0.005;
 
     private final Player player;
 
@@ -51,6 +54,10 @@ public class PlayerCollisionComponent extends CollisionComponent {
 
             if (collisor.entity() instanceof PhysicalPowerup powerup) {
                 player.setPowerup(powerup.getPowerupBehaviour());
+            }
+
+            if (collisor.side() == CollisionSide.TOP) {
+                this.player.setVelocity(player.getVelocity().copyWithY(PUSH_DOWN_FORCE));
             }
         }
     }
