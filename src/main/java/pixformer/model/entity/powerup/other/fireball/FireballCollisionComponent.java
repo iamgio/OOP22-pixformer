1dbffc5dc328066777de8811e448d0f447dd6c62
IGieckI <giacomo.antonelli3@gmail.com>
2023-03-22 14:59:35 +0100

    Lifetime time added to Fireball

--- src/main/java/pixformer/model/entity/powerup/other/fireball/FireballCollisionComponent.java
+++ src/main/java/pixformer/model/entity/powerup/other/fireball/FireballCollisionComponent.java
@@ -2,6 +2,7 @@ package pixformer.model.entity.powerup.other.fireball;
 
 import java.util.Set;
 
+import pixformer.common.time.ChronometerImpl;
 import pixformer.model.World;
 import pixformer.model.entity.collision.Collision;
 import pixformer.model.entity.collision.CollisionComponent;
@@ -11,8 +12,12 @@ import pixformer.model.entity.dynamic.player.Player;
  * Implementation of CollisionComponent for a Player entity.
  */
 public class FireballCollisionComponent extends CollisionComponent {
+    static final long ALIVE_TIME = 3000;
+
     private final Fireball fireball;
     private final World world;
+
+    private final ChronometerImpl despawnCronometer = new ChronometerImpl();
 
     /**
      * 
@@ -23,6 +28,9 @@ public class FireballCollisionComponent extends CollisionComponent {
         super(fireball);
         this.fireball = fireball;
         this.world = world;
+
+        despawnCronometer.reset();
+        despawnCronometer.start();
     }
 
     /**
@@ -30,6 +38,10 @@ public class FireballCollisionComponent extends CollisionComponent {
      */
     @Override
     public void update(final double dt, final Set<Collision> collisions) {
+
+        if (despawnCronometer.hasElapsed(ALIVE_TIME)) {
+            world.dropEntity(fireball);
+        }
 
         for (final var collisor : collisions) {
             if (collisor.side().isHorizontal() && !(collisor.entity() instanceof Player)) {
