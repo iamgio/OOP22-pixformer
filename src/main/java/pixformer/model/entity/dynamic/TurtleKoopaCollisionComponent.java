698c0cdaba1b0d6981fec2e37dc1f31a16058d2e
Luca Patrignani <luca.patrignani3@studio.unibo.it>
2023-03-23 18:46:49 +0100

    Add docs

--- src/main/java/pixformer/model/entity/dynamic/TurtleKoopaCollisionComponent.java
+++ src/main/java/pixformer/model/entity/dynamic/TurtleKoopaCollisionComponent.java
@@ -8,9 +8,17 @@ import pixformer.model.entity.collision.CollisionComponent;
 import java.util.Set;
 import java.util.function.Consumer;
 
+/**
+ * The collision component for a turtle koopa. The koopa dies when pressed by a player.
+ */
 public class TurtleKoopaCollisionComponent extends CollisionComponent {
 
     private final CollisionComponent forDying;
+
+    /**
+     * @param entity to be controlled.
+     * @param die when called it kills the koopa. It consumes the killer.
+     */
     public TurtleKoopaCollisionComponent(final MutableEntity entity, final Consumer<Entity> die) {
         super(entity);
         forDying = new ActionOnPressedCollisionComponent(entity, (__, player) -> die.accept(player));
