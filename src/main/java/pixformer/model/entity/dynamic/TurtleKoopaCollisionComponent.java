1e756da6a92a796b91af2423a43b98b67958c94f
Luca Patrignani <luca.patrignani3@studio.unibo.it>
2023-03-23 18:31:39 +0100

    Create TurtleKoopaCollisionComponent

--- src/main/java/pixformer/model/entity/dynamic/TurtleKoopaCollisionComponent.java
+++ src/main/java/pixformer/model/entity/dynamic/TurtleKoopaCollisionComponent.java
@@ -1,2 +1,29 @@
-package pixformer.model.entity.dynamic;public class TurtleKoopaCollisionComponent {
+package pixformer.model.entity.dynamic;
+
+import pixformer.common.Vector2D;
+import pixformer.model.entity.Entity;
+import pixformer.model.entity.MutableEntity;
+import pixformer.model.entity.collision.BiConsumerCollisionReactor;
+import pixformer.model.entity.collision.Collision;
+import pixformer.model.entity.collision.CollisionComponent;
+import pixformer.model.entity.dynamic.ai.GoombaAI;
+import pixformer.model.entity.dynamic.player.Player;
+
+import java.util.Map;
+import java.util.Set;
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
+
+public class TurtleKoopaCollisionComponent extends CollisionComponent {
+
+    private final CollisionComponent forDying;
+    public TurtleKoopaCollisionComponent(final MutableEntity entity, final Consumer<Entity> die) {
+        super(entity);
+        forDying = new ActionOnPressedCollisionComponent(entity, (__, player) -> die.accept(player));
+    }
+
+    @Override
+    public void update(final double dt, final Set<Collision> collisions) {
+        forDying.update(dt, collisions);
+    }
 }
