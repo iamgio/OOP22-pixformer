1a80d32f1c4a93c2e90f47d82355d2281ef173aa
Luca Patrignani <luca.patrignani3@studio.unibo.it>
2023-03-23 18:18:53 +0100

    Add new test for checking both right and left moving

---
+++ src/main/java/pixformer/model/entity/dynamic/TurtleKoopaCollisionComponent.java
@@ -0,0 +1,2 @@
+package pixformer.model.entity.dynamic;public class TurtleKoopaCollisionComponent {
+}
--- src/test/java/pixformer/TurtleKoopaTest.java
+++ src/test/java/pixformer/TurtleKoopaTest.java
@@ -13,14 +13,25 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 class TurtleKoopaTest {
 
     @Test
-    void testTurtleMovesBecauseOfPlayer() {
+    void testTurtleMovesBecauseOfPlayerLeft() {
         final World world = new WorldImpl(WorldOptionsFactory.testOptions());
-        final TurtleKoopa koopa = new TurtleKoopa(1, 0);
+        final TurtleKoopa koopa = new TurtleKoopa(1, 0, (__) -> { });
         world.spawnEntity(new Player(0, 0, 1, 1, 0));
         world.spawnEntity(koopa);
         assertEquals(koopa.getVelocity().x(), 0);
         world.update(1);
         assertTrue(koopa.getVelocity().x() > 0);
+    }
+
+    @Test
+    void testTurtleMovesBecauseOfPlayerRight() {
+        final World world = new WorldImpl(WorldOptionsFactory.testOptions());
+        final TurtleKoopa koopa = new TurtleKoopa(1, 0, (__) -> { });
+        world.spawnEntity(new Player(2, 0, 1, 1, 0));
+        world.spawnEntity(koopa);
+        assertEquals(koopa.getVelocity().x(), 0);
+        world.update(1);
+        assertTrue(koopa.getVelocity().x() < 0);
     }
 
 }
