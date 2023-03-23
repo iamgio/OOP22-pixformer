11f0ed2dd4c53b4d4bec1856b3b23b046cf79ea2
Luca Patrignani <luca.patrignani3@studio.unibo.it>
2023-03-23 18:46:36 +0100

    Remove unused import

--- src/main/java/pixformer/model/entity/dynamic/TurtleKoopaCollisionComponent.java
+++ src/main/java/pixformer/model/entity/dynamic/TurtleKoopaCollisionComponent.java
@@ -1,17 +1,11 @@
 package pixformer.model.entity.dynamic;
 
-import pixformer.common.Vector2D;
 import pixformer.model.entity.Entity;
 import pixformer.model.entity.MutableEntity;
-import pixformer.model.entity.collision.BiConsumerCollisionReactor;
 import pixformer.model.entity.collision.Collision;
 import pixformer.model.entity.collision.CollisionComponent;
-import pixformer.model.entity.dynamic.ai.GoombaAI;
-import pixformer.model.entity.dynamic.player.Player;
 
-import java.util.Map;
 import java.util.Set;
-import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 
 public class TurtleKoopaCollisionComponent extends CollisionComponent {
