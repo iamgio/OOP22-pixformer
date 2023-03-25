7712e1e5a8c27bbddd4369cec867106e9b792ba9
IGieckI <giacomo.antonelli3@gmail.com>
2023-03-25 19:57:33 +0100

    Implemented FireGraphicsComponent

--- src/main/java/pixformer/view/entity/powerups/fireball/FireballGraphicsComponent.java
+++ src/main/java/pixformer/view/entity/powerups/fireball/FireballGraphicsComponent.java
@@ -1,36 +1,36 @@
 package pixformer.view.entity.powerups.fireball;
 
+import java.util.List;
+
 import pixformer.model.entity.GraphicsComponent;
 import pixformer.model.entity.powerup.other.fireball.Fireball;
 import pixformer.view.engine.Color;
 import pixformer.view.engine.GameScene;
 import pixformer.view.engine.RectangleRenderer;
+import pixformer.view.engine.Renderer;
+import pixformer.view.engine.RendererFactory;
+import pixformer.view.entity.CachedAnimatedGraphicsComponent;
 
 /**
  * Implementation of a GraphicsCopmponent for a Fireball entity.
  */
-public class FireballGraphicsComponent extends GraphicsComponent {
-    private final double width;
-    private final double height;
+public class FireballGraphicsComponent extends CachedAnimatedGraphicsComponent {
+    private static final long SWITCH_TIME = 200;
 
     /**
      * @param fireball Entity that will be displayed.
      */
     public FireballGraphicsComponent(final Fireball fireball) {
-        super(fireball);
-
-        this.width = fireball.getWidth();
-        this.height = fireball.getHeight();
+        super(fireball, SWITCH_TIME);
     }
 
-    /**
-     * {@inheritDoc}}
-     */
     @Override
-    public void update(final GameScene scene) {
-        final RectangleRenderer fireballShape = scene.getRendererFactory().newRectangle(width, height);
-        fireballShape.setColor(new Color(1, 0, 0));
-        scene.getGraphics().draw(fireballShape);
+    protected List<Renderer> createRenderers(RendererFactory factory) {
+        return List.of(
+            factory.newImage("/sprites/player/fireball_1.png", getEntity().getWidth(), getEntity().getHeight()),
+            factory.newImage("/sprites/player/fireball_2.png", getEntity().getWidth(), getEntity().getHeight()),
+            factory.newImage("/sprites/player/fireball_3.png", getEntity().getWidth(), getEntity().getHeight())
+        );
     }
 
 }
