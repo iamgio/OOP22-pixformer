c6f79d6c8512c6185259d6b43c5e946249d8f850
IGieckI <giacomo.antonelli3@gmail.com>
2023-03-25 23:47:04 +0100

    Create a factory for Player's states animations

---
+++ src/main/java/pixformer/view/entity/player/PlayerSpriteFactory.java
@@ -0,0 +1,79 @@
+package pixformer.view.entity.player;
+
+import java.util.List;
+
+import pixformer.view.engine.Renderer;
+
+public class PlayerSpriteFactory {
+    public PlayerAnimation getPlayerAnimation() {
+        return new PlayerAnimation() {
+
+            @Override
+            public List<Renderer> getIdleRenderer() {
+                // TODO Auto-generated method stub
+                throw new UnsupportedOperationException("Unimplemented method 'getIdleRenderer'");
+            }
+
+            @Override
+            public List<Renderer> getWalkRenderer() {
+                // TODO Auto-generated method stub
+                throw new UnsupportedOperationException("Unimplemented method 'getWalkRenderer'");
+            }
+
+            @Override
+            public List<Renderer> getRunRenderer() {
+                // TODO Auto-generated method stub
+                throw new UnsupportedOperationException("Unimplemented method 'getRunRenderer'");
+            }
+
+        };
+    }
+
+    public PlayerAnimation getMushroomPlayerAnimation() {
+        return new PlayerAnimation() {
+
+            @Override
+            public List<Renderer> getIdleRenderer() {
+                // TODO Auto-generated method stub
+                throw new UnsupportedOperationException("Unimplemented method 'getIdleRenderer'");
+            }
+
+            @Override
+            public List<Renderer> getWalkRenderer() {
+                // TODO Auto-generated method stub
+                throw new UnsupportedOperationException("Unimplemented method 'getWalkRenderer'");
+            }
+
+            @Override
+            public List<Renderer> getRunRenderer() {
+                // TODO Auto-generated method stub
+                throw new UnsupportedOperationException("Unimplemented method 'getRunRenderer'");
+            }
+
+        };
+    }
+
+    public PlayerAnimation getFireFlowerPlayerAnimation() {
+        return new PlayerAnimation() {
+
+            @Override
+            public List<Renderer> getIdleRenderer() {
+                // TODO Auto-generated method stub
+                throw new UnsupportedOperationException("Unimplemented method 'getIdleRenderer'");
+            }
+
+            @Override
+            public List<Renderer> getWalkRenderer() {
+                // TODO Auto-generated method stub
+                throw new UnsupportedOperationException("Unimplemented method 'getWalkRenderer'");
+            }
+
+            @Override
+            public List<Renderer> getRunRenderer() {
+                // TODO Auto-generated method stub
+                throw new UnsupportedOperationException("Unimplemented method 'getRunRenderer'");
+            }
+            
+        };
+    }
+}
