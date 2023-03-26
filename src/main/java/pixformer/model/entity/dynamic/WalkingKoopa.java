89299e3a7ce00b1da3fe2db3e16421122ac0ebd8
Luca Patrignani <luca.patrignani3@studio.unibo.it>
2023-03-26 13:17:01 +0200

    Make the walking koopa spawn the turtle koopa when presssed

--- src/main/java/pixformer/model/entity/dynamic/WalkingKoopa.java
+++ src/main/java/pixformer/model/entity/dynamic/WalkingKoopa.java
@@ -1,7 +1,9 @@
 package pixformer.model.entity.dynamic;
 
 import java.util.Optional;
+import java.util.function.BiConsumer;
 
+import pixformer.model.entity.Entity;
 import pixformer.model.entity.collision.CollisionComponent;
 
 /**
@@ -13,17 +15,23 @@ public final class WalkingKoopa extends EnemyImpl implements KoopaState {
     private static final double WIDTH = 1;
     private static final double HEIGHT = 2;
     private final CollisionComponent collisionComponent;
+    private final BiConsumer<Double, Double> turtleFactory;
+    private final BiConsumer<Entity, Entity> die;
 
     /**
      * Construct a WalkingKoopa.
-     * 
-     * @param x              its initial x position
-     * @param y              its initial x position.
-     * @param changeToTurtle a Runnable which makes the koopa becomes a turtle.
+     *
+     * @param x             its initial x position
+     * @param y             its initial x position.
+     * @param turtleFactory for creating a turtle koopa.
+     * @param die           for killing the entity.
      */
-    public WalkingKoopa(final double x, final double y, final Runnable changeToTurtle) {
+    public WalkingKoopa(final double x, final double y, final BiConsumer<Double, Double> turtleFactory,
+                        final BiConsumer<Entity, Entity> die) {
         super(x, y, WIDTH, HEIGHT, INITIAL_VELOCITY);
-        this.collisionComponent = new ActionOnPressedCollisionComponent(this, e -> changeToTurtle.run());
+        this.turtleFactory = turtleFactory;
+        this.die = die;
+        this.collisionComponent = new ActionOnPressedCollisionComponent(this, this::changeToTurtle);
     }
 
     @Override
@@ -34,6 +42,11 @@ public final class WalkingKoopa extends EnemyImpl implements KoopaState {
     @Override
     public boolean isWalking() {
         return true;
+    }
+
+    private void changeToTurtle(final Entity killer) {
+        this.die.accept(this, killer);
+        turtleFactory.accept(getX(), getY() + getHeight() - 1);
     }
 
 }
