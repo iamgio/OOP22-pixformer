2084a4b576f3a46c2138fb4671a734dca65bc9aa
IGieckI <giacomo.antonelli3@gmail.com>
2023-03-26 14:22:45 +0200

    Change parameter type in PlayerState

--- src/main/java/pixformer/view/entity/player/PlayerState.java
+++ src/main/java/pixformer/view/entity/player/PlayerState.java
@@ -3,8 +3,8 @@ package pixformer.view.entity.player;
 import java.util.Optional;
 
 import pixformer.common.Vector2D;
-import pixformer.model.entity.powerup.PowerUp;
+import pixformer.model.entity.powerup.PowerupBehaviour;
 
-public record PlayerState(Optional<PowerUp> powerup, Vector2D velocity) {
+public record PlayerState(Optional<PowerupBehaviour> powerup, Vector2D velocity) {
     
 }
