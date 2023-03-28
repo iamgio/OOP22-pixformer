a5e430ec38ce82e9ba5f90dcff663af8b79d4b5d
Luca Patrignani <luca.patrignani3@studio.unibo.it>
2023-03-28 09:35:16 +0200

    Create DieForFireCollisionReactor

---
+++ src/main/java/pixformer/model/entity/dynamic/reactor/DieForFireCollisionReactor.java
@@ -0,0 +1,31 @@
+package pixformer.model.entity.dynamic.reactor;
+
+import pixformer.model.entity.Entity;
+import pixformer.model.entity.collision.Collision;
+import pixformer.model.entity.collision.CollisionReactor;
+import pixformer.model.entity.powerup.other.fireball.Fireball;
+
+import java.util.Collection;
+import java.util.function.Consumer;
+
+/**
+ * A collision reactor that makes the entity die when hit by a fireball.
+ */
+public class DieForFireCollisionReactor implements CollisionReactor {
+
+    private final CollisionReactor inner;
+
+    /**
+     * @param dieBy consume the killer entity and kill the entity.
+     */
+    public DieForFireCollisionReactor(final Consumer<Entity> dieBy) {
+        inner = new SingleCollisionReactor(
+                collision -> collision.entity() instanceof Fireball, dieBy
+        );
+    }
+
+    @Override
+    public void react(final Collection<Collision> collisions) {
+        inner.react(collisions);
+    }
+}
