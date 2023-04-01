3c25a9b39baf49245490de9860c0f1b10a1ac550
Luca Patrignani <luca.patrignani3@studio.unibo.it>
2023-04-01 14:16:38 +0200

    Use StopWhenPressedCollisionReactor in turtle koopa

--- src/main/java/pixformer/model/entity/dynamic/enemy/koopa/turtle/TurtleKoopaCollisionComponent.java
+++ src/main/java/pixformer/model/entity/dynamic/enemy/koopa/turtle/TurtleKoopaCollisionComponent.java
@@ -9,6 +9,7 @@ import pixformer.model.entity.dynamic.reactor.CollisionReactorFactory;
 import pixformer.model.entity.dynamic.reactor.DieByTurtleCollisionReactor;
 import pixformer.model.entity.dynamic.reactor.DieForFireCollisionReactor;
 import pixformer.model.entity.dynamic.reactor.MakeJumpOnPressedCollisionReactor;
+import pixformer.model.entity.dynamic.reactor.StopWhenPressedCollisionReactor;
 
 import java.util.Set;
 import java.util.function.Consumer;
@@ -29,7 +30,8 @@ public final class TurtleKoopaCollisionComponent extends SolidCollisionComponent
         reactor = CollisionReactorFactory.compose(Set.of(
                 new DieByTurtleCollisionReactor(die),
                 new DieForFireCollisionReactor(die),
-                new MakeJumpOnPressedCollisionReactor()
+                new MakeJumpOnPressedCollisionReactor(),
+                new StopWhenPressedCollisionReactor(x -> entity.setVelocity(entity.getVelocity().copyWithX(x)))
         ));
     }
 
