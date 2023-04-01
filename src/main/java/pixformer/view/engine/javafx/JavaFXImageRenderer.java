fc32379d21c319bd0c04cfa43cd3ebeaf0a42de1
Giorgio Garofalo <giogar2705@gmail.com>
2023-04-01 13:21:32 +0200

    Fix wrong X coordinate of flipped images

--- src/main/java/pixformer/view/engine/javafx/JavaFXImageRenderer.java
+++ src/main/java/pixformer/view/engine/javafx/JavaFXImageRenderer.java
@@ -12,20 +12,23 @@ public class JavaFXImageRenderer extends PositionableRenderer {
     private final Image image;
     private final double width;
     private final double height;
+    private final boolean flipX;
 
     /**
      * Creates a static image renderer for JavaFX.
      * @param image image to render
      * @param width image width
      * @param height image height
+     * @param flipX whether the image should be flipped horizontally
      */
     public JavaFXImageRenderer(
             final Image image,
-            final double width, final double height
+            final double width, final double height, final boolean flipX
     ) {
         this.image = image;
         this.width = width;
         this.height = height;
+        this.flipX = flipX;
     }
 
     /**
@@ -33,7 +36,7 @@ public class JavaFXImageRenderer extends PositionableRenderer {
      * @param image image to render
      */
     public JavaFXImageRenderer(final Image image) {
-        this(image, image.getWidth(), image.getHeight());
+        this(image, image.getWidth(), image.getHeight(), false);
     }
 
     /**
@@ -41,8 +44,11 @@ public class JavaFXImageRenderer extends PositionableRenderer {
      */
     @Override
     public void render(final double dt, final Graphics graphics) {
+        final double x = flipX ? super.getX() + this.width : super.getX();
+        final double width = this.flipX ? -this.width : this.width;
         JavaFXGraphics.requireJFXGraphics(graphics).getGraphics().drawImage(image,
-                super.getX(), super.getY(), this.width, this.height
+                x, super.getY(),
+                width, this.height
         );
     }
 }
