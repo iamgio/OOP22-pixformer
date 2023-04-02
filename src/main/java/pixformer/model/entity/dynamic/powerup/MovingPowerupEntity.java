710412be69971fa9e081017063212d01ab37b108
Luca Patrignani <luca.patrignani3@studio.unibo.it>
2023-04-02 14:30:03 +0200

    Remove unused field

--- src/main/java/pixformer/model/entity/dynamic/powerup/MovingPowerupEntity.java
+++ src/main/java/pixformer/model/entity/dynamic/powerup/MovingPowerupEntity.java
@@ -1,6 +1,5 @@
 package pixformer.model.entity.dynamic.powerup;
 
-import pixformer.common.Vector2D;
 import pixformer.model.entity.AbstractEntity;
 import pixformer.model.entity.DrawableEntity;
 import pixformer.model.entity.Entity;
@@ -8,7 +7,6 @@ import pixformer.model.entity.GraphicsComponent;
 import pixformer.model.entity.GraphicsComponentRetriever;
 import pixformer.model.entity.collision.CollisionComponent;
 import pixformer.model.entity.collision.DefaultRectangleBoundingBoxEntity;
-import pixformer.model.entity.dynamic.OnlyXVelocitySetter;
 import pixformer.model.entity.dynamic.enemy.ai.SimpleAIInputComponent;
 import pixformer.model.entity.powerup.PhysicalPowerup;
 import pixformer.model.entity.powerup.PowerupBehaviour;
@@ -18,7 +16,6 @@ import pixformer.model.physics.PhysicsComponent;
 
 import java.util.Optional;
 import java.util.function.BiConsumer;
-import java.util.function.Consumer;
 
 /**
  * Represents a powerUp entity which moves. It moves like a Goomba but its first direction is right.
@@ -46,7 +43,6 @@ public final class MovingPowerupEntity extends AbstractEntity
     public MovingPowerupEntity(final double x, final double y, final BiConsumer<Entity, Entity> takenBy,
                        final PowerupBehaviour powerupBehaviour, final GraphicsComponentRetriever graphicsComponent) {
         super(x, y, WIDTH, HEIGHT);
-        final Consumer<Vector2D> setter = new OnlyXVelocitySetter(this);
         inputComponent = new SimpleAIInputComponent(
                 this,
                 VELOCITY,
