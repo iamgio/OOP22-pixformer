c302c03d5047ca5ad97e0380edf4e55b221021cf
Luca Patrignani <luca.patrignani3@studio.unibo.it>
2023-04-02 13:51:37 +0200

    Use the new SimpleAIInputComponent in WalkingKoopa

--- src/main/java/pixformer/model/entity/dynamic/enemy/koopa/walking/WalkingKoopa.java
+++ src/main/java/pixformer/model/entity/dynamic/enemy/koopa/walking/WalkingKoopa.java
@@ -8,8 +8,10 @@ import pixformer.model.entity.GraphicsComponentRetriever;
 import pixformer.model.entity.collision.CollisionComponent;
 import pixformer.model.entity.dynamic.enemy.EnemyImpl;
 import pixformer.model.entity.dynamic.enemy.ai.GoombaAI;
+import pixformer.model.entity.dynamic.enemy.ai.SimpleAIInputComponent;
 import pixformer.model.entity.dynamic.enemy.koopa.Koopa;
 import pixformer.model.input.InputComponent;
+import pixformer.model.modelinput.HorizontalModelInput;
 import pixformer.model.physics.PhysicsComponent;
 
 import java.util.Optional;
@@ -48,7 +50,11 @@ public final class WalkingKoopa extends EnemyImpl implements DrawableEntity, Koo
         this.dieBy = killer -> die.accept(this, killer);
         this.collisionComponent = new WalkingKoopaCollisionComponent(this, this::changeToTurtle, dieBy);
         this.graphicsComponent = graphicsComponent.apply(this);
-        this.inputComponent = new GoombaAI(this, super::fixVelocity, INITIAL_VELOCITY);
+        this.inputComponent = new SimpleAIInputComponent(
+                this,
+                INITIAL_VELOCITY,
+                HorizontalModelInput::left
+        );
         this.physicsComponent = new PhysicsComponent(this);
     }
     @Override
