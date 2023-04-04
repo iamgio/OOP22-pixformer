0930dc2cd3c928c92487573bbe04ee7e10d69073
Luca Patrignani <luca.patrignani3@studio.unibo.it>
2023-04-04 16:27:33 +0200

    Create a mock for Player

---
+++ src/test/java/pixformer/PlayerMock.java
@@ -0,0 +1,68 @@
+package pixformer;
+
+import pixformer.model.entity.AbstractEntity;
+import pixformer.model.entity.GraphicsComponent;
+import pixformer.model.entity.collision.DefaultRectangleBoundingBoxEntity;
+import pixformer.model.entity.dynamic.player.Player;
+import pixformer.model.entity.powerup.PowerUp;
+import pixformer.model.entity.powerup.PowerupBehaviour;
+import pixformer.model.physics.PhysicsComponent;
+
+import java.util.Optional;
+
+/**
+ * A mock for Player entity. Use this only for unit tests.
+ */
+class PlayerMock extends AbstractEntity implements Player, DefaultRectangleBoundingBoxEntity {
+    /**
+     * Constructor for the AbstractEntity.
+     *
+     * @param x      X coordinate
+     * @param y      Y coordinate
+     * @param width  width of the entity
+     * @param height height of the entity
+     */
+    PlayerMock(final double x, final double y, final double width, final double height) {
+        super(x, y, width, height);
+    }
+
+    @Override
+    public GraphicsComponent getGraphicsComponent() {
+        return null;
+    }
+
+    @Override
+    public int getIndex() {
+        return 0;
+    }
+
+    @Override
+    public void invulnerable(final long invincibleTime) {
+
+    }
+
+    @Override
+    public boolean isOnGround() {
+        return false;
+    }
+
+    @Override
+    public boolean isTouchingAbove() {
+        return false;
+    }
+
+    @Override
+    public Optional<PowerupBehaviour> getPowerupBehaviour() {
+        return Optional.empty();
+    }
+
+    @Override
+    public PowerUp getPowerup() {
+        return null;
+    }
+
+    @Override
+    public Optional<PhysicsComponent> getPhysicsComponent() {
+        return Optional.of(new PhysicsComponent(this));
+    }
+}
