2341c10d5acb699af44dbf473ef89daccef311a8
IGieckI <giacomo.antonelli3@gmail.com>
2023-04-06 14:42:21 +0200

    PlayerSoundComponent implementation

---
+++ src/main/java/pixformer/model/entity/dynamic/player/PlayerSoundComponent.java
@@ -0,0 +1,28 @@
+package pixformer.model.entity.dynamic.player;
+
+import pixformer.model.entity.Entity;
+import pixformer.model.sound.SoundComponent;
+import pixformer.model.sound.SoundEvent;
+
+public class PlayerSoundComponent extends SoundComponent {
+
+    protected PlayerSoundComponent(Entity entity) {
+        super(entity);
+    }
+
+    public void miniJumpSound() {
+        this.addSound(new SoundEvent("/sounds/entities/mario/common/jump_mini.wav", false));
+    }
+
+    public void bigJumpSound() {
+        this.addSound(new SoundEvent("/sounds/entities/mario/common/jump_big.wav", false));
+    }
+
+    public void getPowerup() {
+        this.addSound(new SoundEvent("/sounds/entities/mario/common/get_powerup.wav", false));
+    }
+
+    public void marioDies() {
+        this.addSound(new SoundEvent("/sounds/entities/mario/common/mario_dies.wav", false));
+    }
+}
